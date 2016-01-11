01361005f1e282ac1013238f80381a1635e96b59
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-11 13:05:20 +0100

    Add tests for error checking.

--- R/conv_mg_n_bio_t.R
+++ R/conv_mg_n_bio_t.R
@@ -46,13 +46,13 @@ extract_param <- function(chars, variable){
 str_split_twice <- function(char, min_only){
   patterns <- c(" ", "\t")
   if (all(!stringr::str_detect(string = char, pattern = patterns))) {
-    stop("Neither space nor tab present in variable char (string)!")
+    stop("Neither space nor tab present.")
   }
   for (i in seq_along(patterns)) {
     char <- unlist(stringr::str_split(string = char, pattern = patterns[i]))
   }
   char <- suppressWarnings(as.numeric(char))
-  if (all(is.na(char))) stop("No numeric value in variable char (string)!")
+  if (all(is.na(char))) stop("No numeric value present.")
   if (min_only) char <- char[min(which(!is.na(char)))]
   return(char)
 }
--- tests/testthat/test_conv_mg_n_bio_t.R
+++ tests/testthat/test_conv_mg_n_bio_t.R
@@ -1,4 +1,4 @@
-context("conv_nmg_to_biom_t test extraction of conversion factor")
+context("get_conv_mgnbiot test extraction of conversion factor")
 
 d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
 prm_biol <- readLines(con = file.path(d, "VMPA_setas_biol_fishing_Trunk.prm"))
@@ -7,6 +7,12 @@ test_that("test conversion factor and helper functions", {
   expect_equal(str_split_twice(char = "test    20", min_only = TRUE), 20)
   expect_equal(str_split_twice(char = "test\t\t\t25", min_only = TRUE), 25)
   expect_equal(str_split_twice(char = "test\t\t    15", min_only = TRUE), 15)
+
+  expect_error(str_split_twice(char = "dadsadadsadadsad", min_only = FALSE), "Neither space nor tab present.")
+  expect_error(str_split_twice(char = "da ds\tdd", min_only = FALSE), "No numeric value present.")
+
+  expect_error(extract_param(chars = c("XXX  15", "YYY   25"), variable = "X_CN"), "Variable X_CN not found.")
+  expect_error(extract_param(chars = c("X_CN  5.7", "X_CN   6.3"), variable = "X_CN"))
 
   expect_equal(extract_param(chars = prm_biol, variable = "X_CN"), 5.7)
   expect_equal(extract_param(chars = prm_biol, variable = "KMIGa_INVERT_ZG"), 1)
