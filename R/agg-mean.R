25dad229e3dad366efa0cf78c40340ecdc8d8bd9
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-12 12:48:30 +0100

    Change documenation. Export agg_sum.

--- R/agg-mean.R
+++ R/agg-mean.R
@@ -1,13 +1,30 @@
-agg_mean <- function(data, col = "atoutput", vars){
-  dots = sapply(vars, . %>% {as.formula(paste0('~', .))})
+#' Aggregate data using dplyr functionality.
+#'
+#' This function is a 'wrapper' for the group_by and summarise procedure
+#' used in dplyr to aggregate dataframes.
+#'
+#' @param data Dataframe the aggregation is applied to.
+#' @param col Column of the dataframe the summarise function is applied.
+#' Default is 'atoutput'.
+#' @param groups Vector of character strings giving the grouping variables.
+#' @return grouped datarame with the aggregated data.
+#' @export
+#'
+#' @examples
+#' agg_ref_nums <- agg_mean(data = ref_nums, groups = c("species", "agecl"))
+
+agg_mean <- function(data, col = "atoutput", groups){
+  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
   result <- as.data.frame(data) %>%
     dplyr::group_by_(.dots = dots) %>%
     dplyr::summarise_(atoutput = lazyeval::interp(~mean(var), var = as.name(col)))
   return(result)
 }
 
-agg_sum <- function(data, col = "atoutput", vars){
-  dots = sapply(vars, . %>% {as.formula(paste0('~', .))})
+#' @export
+#' @rdname agg_mean
+agg_sum <- function(data, col = "atoutput", groups){
+  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
   result <- as.data.frame(data) %>%
     dplyr::group_by_(.dots = dots) %>%
     dplyr::summarise_(atoutput = lazyeval::interp(~sum(var), var = as.name(col)))
