e485dc7bf903accb5972db768774fe60a00b4d0c
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-12 12:53:37 +0100

    Add test for agg_mean and agg_sum.

---
+++ tests/testthat/test-agg-mean.R
@@ -0,0 +1,7 @@
+contect("agg_mean and agg_sum are tested.")
+
+data("ref_nums.rda")
+
+test_that("test agg_mean with reference data for numbers", {
+  expect_equal(dim(agg_mean(data = ref_nums, groups = c("agecl")))[1], 10)
+})
