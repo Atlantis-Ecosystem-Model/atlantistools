6cf1fbcd1498506f3b36498b7991939744964d49
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-12 13:21:21 +0100

    Add type and unit test for agg_mean & agg_sum.

--- tests/testthat/test-agg-mean.R
+++ tests/testthat/test-agg-mean.R
@@ -3,5 +3,10 @@ context("agg_mean and agg_sum are tested.")
 data("ref_nums.rda")
 
 test_that("test agg_mean with reference data for numbers", {
+  expect_is(agg_sum(data = ref_nums, groups = c("species", "agecl")), "data.frame")
   expect_equal(dim(agg_mean(data = ref_nums, groups = c("agecl")))[1], 10)
+  expect_equal(dim(agg_sum(data = ref_nums, groups = c("agecl")))[1], 10)
+  expect_equal(agg_mean(data = ref_nums, col = "agecl", groups = "agecl")$atoutput, 1:10)
 })
+
+
