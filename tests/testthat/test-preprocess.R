29e0c521f5345239c873e78a049fb10f4d210ec7
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-13 12:03:49 +0100

    Add 1st draft of test-preprocess.

--- tests/testthat/test-preprocess.R
+++ tests/testthat/test-preprocess.R
@@ -1,3 +1,9 @@
 context("preprocess test data structure and format")
 
+load(system.file("extdata", "setas-model-new-trunk", "preprocess.Rda", package = "atlantistools"))
+
+test_that("test output numbers", {
+  expect_equal(length(result), 12)
+})
+
 
