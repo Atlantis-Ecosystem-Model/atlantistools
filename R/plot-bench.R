25e1160a991119ca34a2f93e47c0cfdfadfd8d38
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-14 10:56:38 +0100

    Add skeleton for 'plot_bench' function.

---
+++ R/plot-bench.R
@@ -0,0 +1,13 @@
+#' Compare Atlantis scenario with other models/data sources.
+#'
+#' @param data Dataframe to be plotted.
+#' @param
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' plot_bench(preprocess_setas$structn_age)
+
+plot_bench <- function(data, dir, compare) {
+
+}
