3e7e1f1caa34f27f9e88ed52ed26394119e49142
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-14 13:05:28 +0100

    Change input for plot_bench.
    Plot_bench now expects two dataframes. The dataframe with the atlantis run and the external dataframe the
    atlantis run is caompared with.

--- R/plot-bench.R
+++ R/plot-bench.R
@@ -1,15 +1,36 @@
 #' Compare Atlantis scenario with other models/data sources.
 #'
 #' @param data Dataframe to be plotted.
-#' @param dir Character string giving the path of the Atlantis model folder.
-#' If data is stored in multiple folders (e.g. main model folder and output
-#' folder) you should use 'NULL' as dir.
+#' @param ex_data Dataframe to compare the atlantis run with.
 #' @return ggplot2 object
 #' @export
 #'
 #' @examples
-#' plot_bench(preprocess_setas$structn_age)
+#' data_comp <- preprocess_setas$biomass
+#' data_comp$biomass <- data_comp$atoutput * runif(n = nrow(data_comp), min = 0.8, max = 1.2)
+#' data_comp$atoutput <- NULL
+#' data_comp$model <- "test_model"
+#' plot_bench(data = preprocess_setas$biomass, ex_data = data_comp)
 
-plot_bench <- function(data, dir, compare) {
+plot_bench <- function(data, ex_data) {
+  names(ex_data)[names(ex_data) == "biomass"] <- "atoutput"
+  ex_data$time <- as.Date(x = ex_data$time, format = "%Y-%m-%d")
 
+  data$model <- "atlantis"
+  comp <- rbind(ex_data, data)
+
+  comp$model <- factor(comp$model, levels = c("atlantis", sort(unique(comp$model))[sort(unique(comp$model)) != "atlantis"]))
+  model_time <- comp %>%
+    dplyr::group_by_("model") %>%
+    dplyr::summarise_(time = ~max(time))
+  max_time <- min(model_time$time)
+
+  comp <- subset(comp, time <= max_time)
+
+  plot <- ggplot2::ggplot(comp, ggplot2::aes_(x = ~time, y = ~atoutput, colour = ~model)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap(~species, scales = "free_y", ncol = 8) +
+    theme_atlantis()
+
+  return(plot)
 }
