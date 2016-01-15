21699989c2efad024ba62e502bb84e7cb4760660
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-15 14:22:57 +0100

    Add 1st draft of plot_diet

---
+++ R/plot-diet.R
@@ -0,0 +1,16 @@
+#' Plot contribuition of diet contents for each functional group.
+#'
+#' @param dir Character string giving the path of the Atlantis output folder.
+#' @param dietcheck Character string of the DietCheck.txt file. Usually
+#' 'output[...]DietCheck.txt'.
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
+#' plot_diet(dir = d, dietcheck = "outputSETASDietCheck.txt")
+
+plot_diet <- function(dir = getwd(), dietcheck) {
+
+  return(plot)
+}
