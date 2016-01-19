d3eef04791973affd99cbeb6fe3be6b9e3d3878a
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-19 09:51:57 +0100

    Add function to standardise names of functionalgrps.

---
+++ R/convert-factor.R
@@ -0,0 +1,31 @@
+#' Function to convert any column with information about functional groups
+#' to a factor whose levels use the LongName of the functional groups file.
+#'
+#' This function is used to match the labels of the plots!
+#'
+#' @param dir Character string giving the path of the Atlantis model folder.
+#' If data is stored in multiple folders (e.g. main model folder and output
+#' folder) you should use 'NULL' as dir.
+#' @param fgs Character string giving the filename of 'functionalGroups.csv'
+#' file. In case you are using multiple folders for your model files and
+#' outputfiles pass the complete folder/filename string as fgs.
+#' In addition set dir to 'NULL' in this case.
+#' @param col Column of the dataframe which is converted to a factor.
+#' @return Column of a dataframe in factor format.
+#' @export
+#'
+#' @examples
+#'
+
+convert_factor <- function(dir, fgs, col) {
+  fgs <- convert_path(dir = dir, file = fgs)
+
+  # Create vector of sorted labels!
+  labels <- sort(fgs$LongName[is.element(fgs$Code, unique(col))])
+
+  # Convert col to factor!
+  col <- factor(col, levels = fgs$Code[sapply(labels, function(x) which(fgs$LongName == x))], labels = labels)
+
+  return(col)
+}
+
