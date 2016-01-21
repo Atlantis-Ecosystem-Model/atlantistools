058755df5d2ef4bb30e603d5dd71ecbfdfeac660
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-21 11:38:48 +0100

    Change parameterlist.
    The function will only work if load_fgs is performed
    in a previous step. Since this is a helper function
    which is only supposed to work inside other function
    calls or script executions this gives much more
    flexibility than passing dir and filename again.

--- R/convert-factor.R
+++ R/convert-factor.R
@@ -3,33 +3,24 @@
 #'
 #' This function is used to match the labels of the plots!
 #'
-#' @param dir Character string giving the path of the Atlantis model folder.
-#' If data is stored in multiple folders (e.g. main model folder and output
-#' folder) you should use 'NULL' as dir.
-#' @param fgs Character string giving the filename of 'functionalGroups.csv'
-#' file. In case you are using multiple folders for your model files and
-#' outputfiles pass the complete folder/filename string as fgs.
-#' In addition set dir to 'NULL' in this case.
+#' @param data_fgs Dataframe with information about functionalGroups.
+#' Usually loaded with load_fgs().
 #' @param col Column of the dataframe which is converted to a factor.
 #' @return Column of a dataframe in factor format.
 #' @export
-#'
 
-convert_factor <- function(dir, fgs, col) {
-  fgs <- convert_path(dir = dir, file = fgs)
-  fgs <- load_fgs(dir = dir, fgs = fgs)
-
+convert_factor <- function(data_fgs, col) {
   # Create vector of sorted labels!
   avail <- unique(col)
-  if (any(is.element(avail, fgs$Code))) {
-    labels <- sort(fgs$LongName[is.element(fgs$Code, unique(col))])
-    col <- factor(col, levels = fgs$Code[sapply(labels, function(x) which(fgs$LongName == x))], labels = labels)
+  if (all(is.element(avail, data_fgs$Code))) {
+    labels <- sort(data_fgs$LongName[is.element(data_fgs$Code, unique(col))])
+    col <- factor(col, levels = data_fgs$Code[sapply(labels, function(x) which(data_fgs$LongName == x))], labels = labels)
   } else {
-    if (any(is.element(avail, fgs$Name))) {
-       labels <- sort(fgs$LongName[is.element(fgs$Name, unique(col))])
-       col <- factor(col, levels = fgs$Name[sapply(labels, function(x) which(fgs$LongName == x))], labels = labels)
+    if (all(is.element(avail, data_fgs$Name))) {
+       labels <- sort(data_fgs$LongName[is.element(data_fgs$Name, unique(col))])
+       col <- factor(col, levels = data_fgs$Name[sapply(labels, function(x) which(data_fgs$LongName == x))], labels = labels)
     } else {
-      stop("Neither entries in column Code nor Name match the entries in parameter col!")
+      stop("Not all entries in column Code (or Name) match the entries in parameter col!")
     }
   }
 
