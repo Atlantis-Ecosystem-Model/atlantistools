916d4afca51e426c0366ba83f721a48d86e9b35d
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-21 11:37:27 +0100

    Add test for convert_factor.

---
+++ tests/testthat/test-convert-factor.R
@@ -0,0 +1,19 @@
+context("convert_factor tests")
+
+d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
+file <- "functionalGroups.csv"
+
+fgs <- load_fgs(dir = d, fgs = file)
+
+newcol <- convert_factor(data_fgs = fgs, col = fgs$Name)
+newcol2 <- convert_factor(data_fgs = fgs, col = fgs$Code)
+
+test_that("test convert_factor", {
+  expect_is(fgs$Name, "character")
+  expect_is(fgs$Code, "character")
+  expect_is(fgs$LongName, "character")
+  expect_is(newcol, "factor")
+  expect_equal(newcol, as.factor(fgs$LongName))
+  expect_equal(newcol2, as.factor(fgs$LongName))
+  expect_error(convert_factor(data_fgs = fgs, col = fgs$LongName), "Neither entries in column Code nor Name match the entries in parameter col!")
+})
