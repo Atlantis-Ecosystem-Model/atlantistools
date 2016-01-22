d669e2127ce307b787308fbecb0830578cac96a0
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-22 15:56:36 +0100

    Add 1st draft of recruitment plot

---
+++ R/plot-rec.R
@@ -0,0 +1,11 @@
+# #' Plot recruitment.
+# #'
+# #' @param data Dataframe to be plotted.
+# #' @return ggplot2 object
+# #' @export
+# #'
+# #' @examples
+# #' d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
+#
+# plot_rec <- function(data) {
+# }
