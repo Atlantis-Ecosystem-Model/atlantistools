f4db17c1e6e3fccacb928b24ab5620017b86e92a
alketh <Alexander.Keth@uni-hamburg.de>
2016-01-22 12:17:03 +0100

    Finalise convert_bgm.

--- R/convert-bgm.R
+++ R/convert-bgm.R
@@ -10,5 +10,39 @@
 convert_bgm <- function(dir, bgm) {
   box <- load_box(dir = dir, bgm = bgm)
 
-  return(box)
+  # Get info of projection used! Some models don't use '+' to split their
+  # arguments in the projection. So we add them here ;)
+  proj_in <- box$projection
+  if (!any(grepl(pattern = "[+]", proj_in))) {
+    proj_in <- strsplit(proj_in, "[[:space:]]")
+    for (i in seq_along(proj_in)) {
+      proj_in[[i]] <- paste0("+", proj_in[[i]])
+    }
+    proj_in <- paste(unlist(proj_in), collapse = " ")
+  }
+
+  n_boxes <- box$nbox
+
+  # Extract appearance if boxes.
+  result <- list()
+  for (i in 1:n_boxes) {
+    result[[i]] <- data.frame(box$boxes[[i]]$vert)
+    names(result[[i]]) <- c("lat", "long")
+    result[[i]]$inside_lat <- box$boxes[[i]]$inside[1]
+    result[[i]]$inside_long <- box$boxes[[i]]$inside[2]
+    result[[i]]$box <- i - 1
+  }
+  result <- do.call(rbind, result)
+
+  # Convert coordinates to map-coordinates!
+  lat_long <- proj4::project(result[, 1:2], proj = proj_in, inverse = T)
+  result$lat <- lat_long$x
+  result$long <- lat_long$y
+  lat_long <- proj4::project(result[, 3:4], proj = proj_in, inverse = T)
+  result$inside_lat <- lat_long$x
+  result$inside_long <- lat_long$y
+
+  return(result)
 }
+
+
