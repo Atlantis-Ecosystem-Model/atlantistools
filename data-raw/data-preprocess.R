0c6a7816076569b1580f9fd7ee97c48b6ba0b8dc
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-01 16:04:03 +0100

    Properly document creation of dummydataframes.

--- data-raw/data-preprocess.R
+++ data-raw/data-preprocess.R
@@ -1,21 +1,71 @@
-d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
+d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
+
+boundary_boxes <- get_boundary(boxinfo = load_box(dir = d, bgm = "VMPA_setas.bgm"))
+epibenthic_groups <- load_bps(dir = d, fgs = "SETasGroups.csv", init = "init_vmpa_setas_25032013.nc")
+groups <- c("Planktiv_S_Fish", "Pisciv_S_Fish", "Cephalopod", "Megazoobenthos", "Diatom", "Zoo", "Lab_Det", "Ref_Det")
+
+ref_eat <- load_nc(dir = d,
+                   nc = "outputSETASPROD.nc",
+                   bps = epibenthic_groups,
+                   fgs = "SETasGroups.csv",
+                   select_groups = groups,
+                   select_variable = "Eat",
+                   bboxes = boundary_boxes,
+                   check_acronyms = TRUE)
+
+devtools::use_data(ref_eat, pkg = "atlantistools", overwrite = TRUE)
+
+
+ref_grazing <- load_nc(dir = d,
+                       nc = "outputSETASPROD.nc",
+                       bps = epibenthic_groups,
+                       fgs = "SETasGroups.csv",
+                       select_groups = groups,
+                       select_variable = "Grazing",
+                       bboxes = boundary_boxes,
+                       check_acronyms = TRUE)
+
+devtools::use_data(ref_grazing, pkg = "atlantistools", overwrite = TRUE)
+
+
+ref_n <- load_nc(dir = d,
+                 nc = "outputSETAS.nc",
+                 bps = epibenthic_groups,
+                 fgs = "SETasGroups.csv",
+                 select_groups = groups,
+                 select_variable = "N",
+                 bboxes = boundary_boxes,
+                 check_acronyms = TRUE)
+
+devtools::use_data(ref_n, pkg = "atlantistools", overwrite = TRUE)
+
+
+ref_nums <- load_nc(dir = d,
+                    nc = "outputSETAS.nc",
+                    bps = epibenthic_groups,
+                    fgs = "SETasGroups.csv",
+                    select_groups = groups,
+                    select_variable = "Nums",
+                    bboxes = boundary_boxes,
+                    check_acronyms = TRUE)
+
+devtools::use_data(ref_nums, pkg = "atlantistools", overwrite = TRUE)
+
 
 preprocess_setas <- preprocess(dir = d,
    nc_gen = "outputSETAS.nc",
    nc_prod = "outputSETASPROD.nc",
-   prm_biol = "VMPA_setas_biol_fishing_Trunk.prm",
-   prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
-   bps = load_bps(dir = d, fgs = "functionalGroups.csv", init = "INIT_VMPA_Jan2015.nc"),
-   fgs = "functionalGroups.csv",
-   select_groups = get_groups(dir = d, fgs = "functionalGroups.csv"),
-   bboxes = get_boundary(boxinfo = load_box(dir = d, bgm = "VMPA_setas.bgm")),
+   prm_biol = "VMPA_setas_biol_fishing_New.prm",
+   prm_run = "VMPA_setas_run_fishing_F_New.prm",
+   bps = epibenthic_groups,
+   fgs = "SETasGroups.csv",
+   select_groups = groups,
+   bboxes = boundary_boxes,
    check_acronyms = TRUE,
    modelstart = "1991-01-01",
    out = "preprocess.Rda",
    report = TRUE,
-   save_to_disc = TRUE)
-
-
+   save_to_disc = FALSE)
 
 devtools::use_data(preprocess_setas, pkg = "atlantistools", overwrite = TRUE)
 
