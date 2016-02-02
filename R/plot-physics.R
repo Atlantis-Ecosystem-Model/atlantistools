514eb25c373c6d4d85a2866de239cc8b7113da55
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-02 14:55:18 +0100

    Add 1st draft to plot physics.
    This function is derived from plot_physics in the legacy version of the package.

---
+++ R/plot-physics.R
@@ -0,0 +1,31 @@
+#' Function to plot ncdf output of physical variables.
+#'
+#' @param data Dataframe to be plotted.
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' plot_physics(preprocess_setas$physics)
+
+plot_physics <- function(data) {
+  if (!any(is.element(names(data), "time"))) {
+    stop("Column time not found in data")
+  }
+
+  plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput)) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap(~species, scales = "free_y", ncol = 8, labeller = ggplot2::label_wrap_gen(width = 15)) +
+    ggplot2::guides(col = ggplot2::guide_legend(nrow = 1)) +
+    theme_atlantis()
+
+  # Allow plotting for both cohort and non-cohort data!
+  if (is.element("agecl", names(data))) {
+    plot <- plot + ggplot2::aes_(colour = ~factor(agecl))
+  }
+
+  return(plot)
+}
+
+
+
+
