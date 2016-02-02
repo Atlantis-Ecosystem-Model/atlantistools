515d5fd79dea81386b83f26859e33c01556dd3c0
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-02 14:54:33 +0100

    Add function to check data structure in plot routines.

---
+++ R/check-df-names.R
@@ -0,0 +1,27 @@
+#' Function to check the names of a dataframe.
+#'
+#' This function is used in most plotting routines to check if the correct dataframe
+#' is passed.
+#'
+#' @param data Dataframe to check.
+#' @param expect Character vector giving the names of the expected columns
+#' @param optional Character vector giving the names of optional columns.
+#' @export
+#'
+#' @examples \dontrun{
+#' check_df_names(preprocess_setas$biomass_age, expect = c("time", "species", "atoutput", "ages"))
+#' }
+
+check_df_names <- function(data, expect, optional = NULL) {
+  df_names <- names(data)
+  expect <- c(expect, optional)
+  wrong_names <- df_names[!is.element(df_names, expect)]
+
+  if (length(wrong_names) > 1) {
+    stop(paste("Columns", paste(wrong_names, collapse = " & "), "not found in data."))
+  }
+}
+
+
+
+
