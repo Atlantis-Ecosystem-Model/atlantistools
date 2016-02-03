2d750c22001cfd0cfef4cad01161c873314a897d
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-03 15:22:52 +0100

    Add str_split_twice.

---
+++ R/str-split-twice.R
@@ -0,0 +1,28 @@
+#' Extract numeric values from string.
+#'
+#' The function splits any character string at each tab and space and returns
+#' all (min_only = FALSE) or only the first (min_only = T) numeric value found in the string.
+#'
+#' @param char Character string.
+#' @param min_only Logical specifying if only the first numeric value (\code{TRUE}) or
+#' all numeric values (\code{FALSE}) should be returned.
+#' @export
+#'
+#' @examples
+#' str_split_twice(char = "Hello   15")
+
+# Split any character string multiple times and retrun the first (min_only = T)
+# or all (min_only = F) numeric values found.
+str_split_twice <- function(char, min_only = TRUE){
+  patterns <- c(" ", "\t")
+  if (all(!stringr::str_detect(string = char, pattern = patterns))) {
+    stop("Neither space nor tab present.")
+  }
+  for (i in seq_along(patterns)) {
+    char <- unlist(stringr::str_split(string = char, pattern = patterns[i]))
+  }
+  char <- suppressWarnings(as.numeric(char))
+  if (all(is.na(char))) stop("No numeric value present.")
+  if (min_only) char <- char[min(which(!is.na(char)))]
+  return(char)
+}
