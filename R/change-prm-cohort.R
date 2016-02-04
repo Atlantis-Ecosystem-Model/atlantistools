90ebaaea4a6fe9845cf8fdd86f0b2915a12c8bc2
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-04 14:47:47 +0100

    Add change_prm_cohort to update variable for cohort groups.
    This function is mainly based on change_prm with minor
    changes. E.g. variables are stored in the next row.
    Input structure of roc is different.

---
+++ R/change-prm-cohort.R
@@ -0,0 +1,90 @@
+#' Change biological parameterfile for parameters which expect multiple values.
+#'
+#'
+#' This functionis is used to help automate the calibration routine for ATLANTIS models.
+#'
+#' @param dir Character string giving the path of the Atlantis model folder.
+#' If data is stored in multiple folders (e.g. main model folder and output
+#' folder) you should use 'NULL' as dir.
+#' @param prm_biol Character string giving the filename of the biological
+#' parameterfile. Usually "[...]biol_fishing[...].prm". In case you are using
+#' multiple folders for your model files and outputfiles pass the complete
+#' folder/filename string and set dir to 'NULL'.
+#' @param select_acronyms Character vector of funtional groups which shall be read in.
+#' Names have to match the ones used in the *.prm file. Check column "Code" in
+#' "functionalGroups.csv" for clarification.
+#' @param roc Matrix of multiplication factors which shall be applied to the old set of parameters.
+#' Please supply one row per selected group. Each row should have as many entries as the
+#' parameter itself. E.g. if you want to change the clearance rate for two fish groups you
+#' need to supply a matrix with 2 rows and 10 columns.
+#' @param parameter Character value of the model parameter which shall be changed.
+#' Only one parameter can be selected per function call.
+#' @param relative Logical if TRUE values are changed relative to base values. If FALSE new values can
+#' be passed directly.
+#' @param save_to_disc Logical indicating if the resulting prm file should be overwritten
+#' (\code{TRUE}) or not (\code{FALSE}).
+#' @return parameterfile *.prm file with the new parameter values.
+#' @export
+#'
+#' @examples
+#' d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
+#' change_prm_cohort(dir = d,
+#'    prm_biol = "VMPA_setas_biol_fishing_New.prm",
+#'    select_acronyms = c("FPS", "FVS"),
+#'    roc = c(2,3),
+#'    parameter = "C",
+#'    save_to_disc = FALSE)
+
+
+change_prm_cohort <- function(dir = getwd(), prm_biol, select_acronyms, roc, parameter, relative = TRUE, save_to_disc = TRUE) {
+  if (length(parameter) != 1) stop("Please suply only one parameter per function call.")
+
+  # Convert to matrix if only one species is selected and roc is a vector!
+  if (length(select_acronyms) == 1 & is.vector(roc)) roc <- matrix(roc, nrow = 1)
+
+  if (length(select_acronyms) != nrow(roc)) {
+    stop("Dimensions of select_acronyms and roc do not match. Please supply one row of values per group.")
+  }
+
+  # Read in parameter file!
+  prm_biol_new <- convert_path(dir = dir, file = prm_biol)
+  prm_biol_new <- readLines(con = prm_biol_new)
+
+  # Function to update a specific parameter composed of a parameter string
+  # a group acronym and a seperator (by default "_") found in a prm file.
+  update_prm_species <- function(prm_biol, acronym, roc, parameter, relative) {
+    flag <- paste(parameter, acronym, sep = "_")
+    pos <- scan_prm(chars = prm_biol, variable = flag)
+    # Values are stored in the next row in the *.prm file.
+    pos <- pos + 1
+
+    # Keep all numeric values
+    old_value <- str_split_twice(char = prm_biol[pos], min_only = FALSE)
+    if (length(old_value) != length(roc)) {
+      stop(paste(length(old_value), "values found but only", length(roc), "new values supplied."))
+    }
+
+    if (relative) {
+      new_value <- old_value * roc
+    } else {
+      new_value <- roc
+    }
+
+    # Update value. Some pesky expectations have to be added here.
+    prm_biol[pos] <- paste(new_value, collapse = "\t")
+    return(prm_biol)
+  }
+
+  for (i in seq_along(select_acronyms)) {
+    if (!(roc[i, ] == 1 & relative)) {
+      prm_biol_new <- update_prm_species(prm_biol = prm_biol_new, acronym = select_acronyms[i], roc = roc[i, ], parameter = parameter, relative = relative)
+    }
+  }
+
+  if (save_to_disc) {
+    print("Writing new prm file!")
+    writeLines(text = prm_biol_new, con = convert_path(dir = dir, file = prm_biol), sep = "\n")
+  }
+
+  return(prm_biol_new)
+}
