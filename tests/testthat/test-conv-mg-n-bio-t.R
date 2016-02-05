b4360af10261b775951bdd002122e0a01190e895
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-05 12:18:39 +0100

    Move tests for str_split_twice in own file.

--- tests/testthat/test-conv-mg-n-bio-t.R
+++ tests/testthat/test-conv-mg-n-bio-t.R
@@ -4,13 +4,6 @@ d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
 prm_biol <- readLines(con = file.path(d, "VMPA_setas_biol_fishing_New.prm"))
 
 test_that("test conversion factor and helper functions", {
-  expect_equal(str_split_twice(char = "test    20", min_only = TRUE), 20)
-  expect_equal(str_split_twice(char = "test\t\t\t25", min_only = TRUE), 25)
-  expect_equal(str_split_twice(char = "test\t\t    15", min_only = TRUE), 15)
-
-  expect_error(str_split_twice(char = "dadsadadsadadsad", min_only = FALSE), "Neither space nor tab present.")
-  expect_error(str_split_twice(char = "da ds\tdd", min_only = FALSE), "No numeric value present.")
-
   expect_error(extract_prm(chars = c("XXX  15", "YYY   25"), variable = "X_CN"), "Variable X_CN not found.")
   expect_error(extract_prm(chars = c("X_CN  5.7", "X_CN   6.3"), variable = "X_CN"))
 
