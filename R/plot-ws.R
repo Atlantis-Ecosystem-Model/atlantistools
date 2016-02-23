0bd8df291930a691ae51e34fe806693341a6a367
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-23 13:02:10 +0100

    Add theme to plot_ws.

--- R/plot-ws.R
+++ R/plot-ws.R
@@ -12,10 +12,14 @@ plot_ws <- function(data, combine_thresh = 0.03) {
 
   data <- combine_groups(data, group_col = "species", groups = "time", combine_thresh = combine_thresh)
 
+  # Arrange data according to contribution!
+  agg_data <- agg_sum(data, groups = c("time", "species"), out = "sum_at")
+  data$species <- factor(data$species, levels = agg_data$species[order(agg_data$sum_at, decreasing = TRUE)])
+
   plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput, fill = ~species)) +
     ggplot2::geom_bar(stat = "identity") +
     ggplot2::scale_fill_manual(values = get_colpal()) +
-    ggplot2::labs(y = "Biomass [t]")
+    ggplot2::labs(y = "Biomass [t]") +
     theme_atlantis() +
     ggplot2::theme(legend.position = "right")
 
