da131fbc3fca1b3e633826d00e4c8b4a9fb6afc3
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-23 12:09:16 +0100

    Add function to combine values in dataframe.
    First the contribution of the value to the overall values is calculated. If the contribution for a specific group is lower than the submitted threshold the group is transformed to the "Rest" group.

---
+++ R/combine-groups.R
@@ -0,0 +1,26 @@
+#' Combine values from different groups if specific groups only have a low contribution to the overall value.
+#'
+#' @param data Dataframe whose grouops shall be combined.
+#' @param group_col Character string giving the name of the group column in `data`.
+#' @param groups Vector of character strings giving the grouping variables.
+#' @param combine_thresh Numeric indicating minimum amount to the stomach contribution.
+#' Each prey item with a lower contribution as this treshold is assigned to the
+#' grpup "Rest". This is necessary for species with a wide variety of food items
+#' in their diet. Otherwise the screen will be cluttered with colors in the
+#' dietplots. Default is 0.03.
+#' @return dataframe with groupgs combined to "Rest" if contribution is low.
+#' @export
+#'
+#' @examples
+#' combine_groups(preprocess_setas$diet,
+#'    group_col = "prey",
+#'    groups = c("time", "pred", "habitat"),
+#'    combine_thresh = 0.03)
+
+combine_groups <- function(data, group_col, groups, combine_thresh = 0.03) {
+  # Convert values to percent!
+  data <- agg_perc(data, groups = groups, out = "test")
+  data[data$test <= combine_thresh, group_col] <- "Rest"
+  data <- agg_sum(data, groups = c(groups, group_col))
+  return(data)
+}
---
+++ tests/testthat/test-combine-groups.R
@@ -0,0 +1,13 @@
+context("combine_groups tests")
+
+d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
+
+diet <- load_dietcheck(dir = d,
+                       dietcheck = "outputSETASDietCheck.txt")
+
+test_that("test combine_groups", {
+  expect_equal(combine_groups(data = diet, group_col = "prey", groups = c("time", "pred", "habitat"), combine_thresh = 0), diet)
+  expect_is(combine_groups(data = preprocess_setas$biomass, group_col = "species", groups = "time"), "grouped_df")
+  expect_equal(nrow(combine_groups(data = preprocess_setas$biomass_age, group_col = "species", groups = c("time", "agecl"), combine_thresh = 1)), 40)
+  expect_equal(unique(combine_groups(data = preprocess_setas$biomass_age, group_col = "species", groups = c("time", "agecl"), combine_thresh = 1)$species), "Rest")
+})
