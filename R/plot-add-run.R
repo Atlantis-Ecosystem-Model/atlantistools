c2c6c5adf9710654580d2ba2ae66d465c5a30e20
alketh <Alexander.Keth@uni-hamburg.de>
2016-02-25 10:50:18 +0100

    Finalise plot_add_run!

--- R/plot-add-run.R
+++ R/plot-add-run.R
@@ -1,29 +1,34 @@
 #' Low level plotting function to apply color coding to plots based on run variable!
 #'
-#' This function can be used to use plotting routines without color specification
-#' e.g. `plot_ts()`, `plot_calibrate()` or `plot_physics()` for model comparisons.
-#' Please note that `plot_ts()` and `plot_calibrate()` only work for non age based
-#' data. The colorcoding is based on the column run so you need to apply combine runs
-#' to your data first!
+#' This function can be used to add colour coding to plotting routines without color specification
+#' e.g. \code{plot_ts()}, \code{plot_calibrate()} or \code{plot_physics()}. The colorcoding is
+#' based on the column run so you need to apply \code{combine_runs} to your data first!
+#' Please note that \code{plot_ts()} and \code{plot_calibrate()} only work if they are created
+#' with non-age based data.
 #'
 #' @param plot ggplot2 object.
 #' @return ggplot2 plot.
 #' @export
 #'
 #' @examples
-#'
+#' dummy <- preprocess_setas
+#' # Change output of dummy simulation!
+#' dummy$biomass$atoutput <- dummy$biomass$atoutput * 1.5
+#' dfs <- combine_runs(list(preprocess_setas, dummy), runs = c("setas", "dummy"))
+#' plot <- plot_ts(dfs$biomass)
+#' plot <- plot_add_run(plot)
+#' plot
 
-plot <- plot_ts(preprocess_setas$biomass_age)
-plot_add_run <- function()
-if (!("run" %in% names(plot$data)) stop("Variable run not found in plot!")
-if (!ggplot2::is.ggplot(plot)) stop("Provided plot is no ggplot2 plot.")
-if ("colour" %in% names(plot$mapping)) {
-  warning("Color coding in plot already present. You probably don't want to overwrite this with model runs!")
+plot_add_run <- function(plot) {
+  # Check input
+  # Otherwise the error first occurs when the plot is drawn!
+  if (!("run" %in% names(plot$data))) stop("Variable run not found in plot!")
+  if (!ggplot2::is.ggplot(plot)) stop("Plot is no ggplot2 object.")
+  if ("colour" %in% names(plot$mapping)) {
+    warning("Color coding in plot already present. You probably don't want to overwrite this with model 'run'!")
+  }
+  plot <- plot + ggplot2::aes_(colour = ~run)
+  return(plot)
 }
 
-plot <- plot + ggplot2::aes(colour = species)
-
-names(plot$mapping)
-
-plot <- plot + ggplot2::aes_(colour = ~run)
 
