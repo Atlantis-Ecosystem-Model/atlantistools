a9151d10bb8a3ceddfc1a2c1dc9a76109d3d66ab
alketh <Alexander.Keth@uni-hamburg.de>
2016-03-03 10:31:29 +0100

    Switch axes.

--- R/plot-boxes.R
+++ R/plot-boxes.R
@@ -14,11 +14,11 @@ plot_boxes <- function(data) {
 
   inside <- unique(subset(data, select = c("inside_lat", "inside_long", "box")))
   names(inside)[1:2] <- c("lat", "long")
-  plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~long, y = ~lat, fill = ~factor(box), group = ~factor(box), label = ~box)) +
+  plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~lat, y = ~long, fill = ~factor(box), group = ~factor(box), label = ~box)) +
     ggplot2::geom_polygon(colour = "black") +
     ggplot2::geom_text(data = inside) +
     theme_atlantis() +
-    ggplot2::guides(fill = ggplot2::guide_legend(ncol = 1)) +
+    ggplot2::guides(fill = ggplot2::guide_legend(ncol = 3)) +
     ggplot2::theme(legend.position = "right")
 
   return(plot)
