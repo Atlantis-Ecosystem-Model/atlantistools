770344b05cb940eea2c42ecdc41eeb09620c7cfc
alketh <Alexander.Keth@uni-hamburg.de>
2016-03-17 11:31:24 +0100

    Update documentation.

--- R/data-preprocess-setas.R
+++ R/data-preprocess-setas.R
@@ -1,5 +1,5 @@
 #' preprocess-setas
 #'
-#' @format List with 12 dataframes.
+#' @format List with 15 dataframes.
 #' @source See \code{data-raw/data-preprocess.R} for further information.
 "preprocess_setas"
