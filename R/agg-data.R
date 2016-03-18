ae250690f7254a948e7e363a9d2fb0cbf9bd887c
alketh <Alexander.Keth@uni-hamburg.de>
2016-03-18 15:55:17 +0100

    Rewrite aggregation functions.
    Introduce function to group dataframes. Add parameter to pass aggregation function as argument (sum, mean).

--- R/agg-data.R
+++ R/agg-data.R
@@ -12,48 +12,29 @@
 #' @export
 #'
 #' @examples
-#' agg_ref_nums <- agg_mean(data = ref_nums, groups = c("species", "agecl"))
+#' agg_ref_nums <- agg_data(data = ref_nums, groups = c("species", "agecl"), fun = mean)
 
-agg_mean <- function(data, col = "atoutput", groups, out = "atoutput"){
-  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
-  result <- as.data.frame(data) %>%
-    dplyr::group_by_(.dots = dots) %>%
-    dplyr::summarise_(atoutput = lazyeval::interp(~mean(var), var = as.name(col)))
-  if (out != "atoutput") names(result)[names(result) == "atoutput"] <- out
-  return(result)
-}
-
-#' @export
-#' @rdname agg_mean
-agg_sum <- function(data, col = "atoutput", groups, out = "atoutput"){
-  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
-  result <- as.data.frame(data) %>%
-    dplyr::group_by_(.dots = dots) %>%
-    dplyr::summarise_(atoutput = lazyeval::interp(~sum(var), var = as.name(col)))
-  if (out != "atoutput") names(result)[names(result) == "atoutput"] <- out
+agg_data <- function(data, col = "atoutput", groups, out = "atoutput", fun){
+  result <- group_data(data, groups = groups) %>%
+    dplyr::summarise_(.dots = setNames(list(lazyeval::interp(~fun(var), var = as.name(col))), out))
   return(result)
 }
 
 #' @export
 #' @rdname agg_mean
 agg_perc <- function(data, col = "atoutput", groups, out = "atoutput"){
-  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
-  result <- as.data.frame(data) %>%
-    dplyr::group_by_(.dots = dots) %>%
-    dplyr::mutate_(xyz = lazyeval::interp(~var/sum(var), var = as.name(col)))
-  # If output column is not identical with the input column!
-  # WARNING: Reading the following lines may cause health issues... ;)
-  if (out != "atoutput") {
-    names(result)[names(result) == "xyz"] <- out
-  } else {
-    result[, col] <- NULL
-    names(result)[names(result) == "xyz"] <- "atoutput"
-  }
+  result <- group_data(data, groups = groups) %>%
+    dplyr::mutate_(.dots = setNames(list(lazyeval::interp(~var/sum(var), var = as.name(col))), out))
   return(result)
 }
 
 #' @export
 #' @rdname agg_mean
-
+group_data <- function(data, groups) {
+  dots = sapply(groups, . %>% {as.formula(paste0('~', .))})
+  grouped_df <- as.data.frame(data) %>%
+    dplyr::group_by_(.dots = dots)
+  return(grouped_df)
+}
 
 
