54eee8ace259017d36780f6029a46ecc727a19ad
alketh <Alexander.Keth@uni-hamburg.de>
2016-03-18 16:21:16 +0100

    Update test.

--- tests/testthat/test-agg-mean.R
+++ tests/testthat/test-agg-mean.R
@@ -1,13 +1,19 @@
 context("agg_data is tested.")
 
-# data("ref_nums.rda")
+perc <- agg_perc(data = ref_n, groups = "species", out = "test")
 
 test_that("test agg_data with reference data for numbers", {
   expect_is(agg_data(data = ref_nums, groups = c("species", "agecl"), fun = sum), "data.frame")
   expect_equal(dim(agg_data(data = ref_nums, groups = c("agecl"), fun = mean))[1], 10)
   expect_equal(dim(agg_data(data = ref_nums, groups = c("agecl"), fun = sum))[1], 10)
   expect_equal(agg_data(data = ref_nums, col = "agecl", groups = "agecl", fun = mean)$atoutput, 1:10)
+  expect_equal(names(agg_data(data = ref_nums, groups = "species", out = "test", fun = sum)), c("species", "test"))
+  expect_equal(names(perc), c("polygon", "layer", "species", "time", "atoutput", "test"))
+  expect_equal(agg_data(perc, col = "test", groups = "species", fun = sum)$atoutput, rep(1, times = 8))
 })
 
+test_that("test data grouping", {
+  expect_is(group_data(ref_nums, groups = c("species", "agecl")), "grouped_df")
+})
 
 # add commented line
