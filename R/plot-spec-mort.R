0008ecb06201398cb181f1b9e80cbd617320f320
alketh <Alexander.Keth@uni-hamburg.de>
2016-03-30 11:30:54 +0200

    outcomment plot_spec_mort.

--- R/plot-spec-mort.R
+++ R/plot-spec-mort.R
@@ -13,84 +13,84 @@
 #' plots <- plot_spec_mort(data = preprocess_setas$diet_specmort, combine_thresh = 0.03)
 #' gridExtra::grid.arrange(plots[[1]])
 
-plot_spec_mort <- function(data, combine_thresh = 0.03) {
-  check_df_names(data = data, expect = c("time", "pred", "agecl", "prey", "atoutput"))
-
-  # Timeseries per species of absolute values!
-  # ts_diet <- agg_sum(data, groups = c("time", "pred", "habitat"))
-
-  # Combine prey groups with low contribution to the diet!
-  pred <- combine_groups(data,
-                         group_col = "prey",
-                         groups = c("time", "pred", "agecl"),
-                         combine_thresh = combine_thresh)
-
-  prey <- combine_groups(data,
-                         group_col = "pred",
-                         groups = c("time", "prey", "agecl"),
-                         combine_thresh = combine_thresh)
-
-  # Convert diet data to percentages!
-  pred <- agg_perc(pred, groups = c("time", "pred", "agecl"))
-  prey <- agg_perc(prey, groups = c("time", "prey", "agecl"))
-
-  # data$prey[data$atoutput <= combine_thresh] <- "Rest"
-  # data <- agg_sum(data, groups = c("time", "pred", "habitat", "prey"))
-
-  plot_func <- function(data) {
-    # order data according to dietcontribution
-    agg_data <- data %>%
-      dplyr::group_by_(~prey) %>%
-      dplyr::summarise_(sum_diet = ~sum(atoutput))
-    data$prey <- factor(data$prey, levels = agg_data$prey[order(agg_data$sum_diet, decreasing = TRUE)])
-    plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~time, y = ~atoutput, fill = ~prey)) +
-      ggplot2::geom_bar(stat = "identity") +
-      ggplot2::scale_fill_manual(values = get_colpal()) +
-      ggplot2::facet_wrap(~agecl, ncol = 5, labeller = ggplot2::label_wrap_gen(width = 15)) +
-      ggplot2::labs(x = "time", y = "relative M2", title = NULL) +
-      ggplot2::coord_cartesian(expand = FALSE) +
-      ggplot2::labs(title = paste("Predationmortality caused by:", unique(data$pred))) +
-      theme_atlantis() +
-      ggplot2::theme(legend.position = "right")
-
-    return(plot)
-  }
-
-  plot_func2 <- function(data) {
-    # order data according to dietcontribution
-    # agg_data <- data %>%
-    #   dplyr::group_by_(~pred) %>%
-    #   dplyr::summarise_(sum_diet = ~sum(atoutput))
-    # data$pred <- factor(data$pred, levels = agg_data$pred[order(agg_data$sum_diet, decreasing = TRUE)])
-    plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~time, y = ~atoutput, fill = ~pred)) +
-      ggplot2::geom_bar(stat = "identity") +
-      ggplot2::scale_fill_manual(values = get_colpal()) +
-      ggplot2::facet_wrap(~agecl, ncol = 5, labeller = ggplot2::label_wrap_gen(width = 15)) +
-      ggplot2::labs(x = "time", y = "relative M2", title = NULL) +
-      ggplot2::coord_cartesian(expand = FALSE) +
-      ggplot2::labs(title = paste("Predationmortality excerted upon:", unique(data$pred))) +
-      theme_atlantis() +
-      ggplot2::theme(legend.position = "right")
-
-    return(plot)
-  }
-
-  # plot_func_ts <- function(data) {
-  #   plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput)) +
-  #     ggplot2::geom_line() +
-  #     ggplot2::labs(y = "DietCheck absolute") +
-  #     theme_atlantis() +
-  #     ggplot2::coord_cartesian(expand = FALSE)
-  # }
-
-  plots_pred <- lapply(split(pred, pred$pred), plot_func)
-  plots_prey <- lapply(split(prey, prey$prey), plot_func2)
-  # plots_ts <- lapply(split(ts_diet, ts_diet$pred), plot_func_ts)
-
-  grobs <- mapply(gridExtra::arrangeGrob, plots_diet, plots_ts, MoreArgs = list(heights = grid::unit(c(0.7, 0.3), units = "npc")))
-
-  return(grobs)
-}
+# plot_spec_mort <- function(data, combine_thresh = 0.03) {
+#   check_df_names(data = data, expect = c("time", "pred", "agecl", "prey", "atoutput"))
+#
+#   # Timeseries per species of absolute values!
+#   # ts_diet <- agg_sum(data, groups = c("time", "pred", "habitat"))
+#
+#   # Combine prey groups with low contribution to the diet!
+#   pred <- combine_groups(data,
+#                          group_col = "prey",
+#                          groups = c("time", "pred", "agecl"),
+#                          combine_thresh = combine_thresh)
+#
+#   prey <- combine_groups(data,
+#                          group_col = "pred",
+#                          groups = c("time", "prey", "agecl"),
+#                          combine_thresh = combine_thresh)
+#
+#   # Convert diet data to percentages!
+#   pred <- agg_perc(pred, groups = c("time", "pred", "agecl"))
+#   prey <- agg_perc(prey, groups = c("time", "prey", "agecl"))
+#
+#   # data$prey[data$atoutput <= combine_thresh] <- "Rest"
+#   # data <- agg_sum(data, groups = c("time", "pred", "habitat", "prey"))
+#
+#   plot_func <- function(data) {
+#     # order data according to dietcontribution
+#     agg_data <- data %>%
+#       dplyr::group_by_(~prey) %>%
+#       dplyr::summarise_(sum_diet = ~sum(atoutput))
+#     data$prey <- factor(data$prey, levels = agg_data$prey[order(agg_data$sum_diet, decreasing = TRUE)])
+#     plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~time, y = ~atoutput, fill = ~prey)) +
+#       ggplot2::geom_bar(stat = "identity") +
+#       ggplot2::scale_fill_manual(values = get_colpal()) +
+#       ggplot2::facet_wrap(~agecl, ncol = 5, labeller = ggplot2::label_wrap_gen(width = 15)) +
+#       ggplot2::labs(x = "time", y = "relative M2", title = NULL) +
+#       ggplot2::coord_cartesian(expand = FALSE) +
+#       ggplot2::labs(title = paste("Predationmortality caused by:", unique(data$pred))) +
+#       theme_atlantis() +
+#       ggplot2::theme(legend.position = "right")
+#
+#     return(plot)
+#   }
+#
+#   plot_func2 <- function(data) {
+#     # order data according to dietcontribution
+#     # agg_data <- data %>%
+#     #   dplyr::group_by_(~pred) %>%
+#     #   dplyr::summarise_(sum_diet = ~sum(atoutput))
+#     # data$pred <- factor(data$pred, levels = agg_data$pred[order(agg_data$sum_diet, decreasing = TRUE)])
+#     plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~time, y = ~atoutput, fill = ~pred)) +
+#       ggplot2::geom_bar(stat = "identity") +
+#       ggplot2::scale_fill_manual(values = get_colpal()) +
+#       ggplot2::facet_wrap(~agecl, ncol = 5, labeller = ggplot2::label_wrap_gen(width = 15)) +
+#       ggplot2::labs(x = "time", y = "relative M2", title = NULL) +
+#       ggplot2::coord_cartesian(expand = FALSE) +
+#       ggplot2::labs(title = paste("Predationmortality excerted upon:", unique(data$pred))) +
+#       theme_atlantis() +
+#       ggplot2::theme(legend.position = "right")
+#
+#     return(plot)
+#   }
+#
+#   # plot_func_ts <- function(data) {
+#   #   plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput)) +
+#   #     ggplot2::geom_line() +
+#   #     ggplot2::labs(y = "DietCheck absolute") +
+#   #     theme_atlantis() +
+#   #     ggplot2::coord_cartesian(expand = FALSE)
+#   # }
+#
+#   plots_pred <- lapply(split(pred, pred$pred), plot_func)
+#   plots_prey <- lapply(split(prey, prey$prey), plot_func2)
+#   # plots_ts <- lapply(split(ts_diet, ts_diet$pred), plot_func_ts)
+#
+#   grobs <- mapply(gridExtra::arrangeGrob, plots_diet, plots_ts, MoreArgs = list(heights = grid::unit(c(0.7, 0.3), units = "npc")))
+#
+#   return(grobs)
+# }
 
 
 # df <- load_txt(dir = "Z:/Atlantis_models/Runs/dummy_01_ATLANTIS_NS", file = "outputNorthSeaSpecificPredMort.txt")
