1037d44b0eab29939ab45a4d20dacddb4b16372c
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-04-03 14:01:57 +0200

    Restrict available groups to 15 (1st draft).

--- R/combine-groups.R
+++ R/combine-groups.R
@@ -18,27 +18,32 @@
 #'    combine_thresh = 0.03)
 
 combine_groups <- function(data, group_col, groups, combine_thresh = 0.03) {
-  # Convert values to percent!
-  data <- agg_perc(data, groups = groups, out = "test")
+  # # Convert values to percent!
+  # data <- agg_perc(data, groups = groups, out = "test")
+  #
+  # # Get species which always have a low contribution!
+  # ts <- length(unique(data$time))
+  # low_contrib <- data %>%
+  #   dplyr::filter_(~test <= combine_thresh) %>%
+  #   group_data(groups = c(groups[groups != "time"], group_col)) %>%
+  #   dplyr::summarise_(count = ~length(unique(time))) %>%
+  #   dplyr::filter_(~count == ts)
+  # low_contrib$count <- NULL
+  #
+  # # Get position of merge between data and low_contrib
+  # full_match <- matrix(nrow = nrow(data), ncol = ncol(low_contrib))
+  # col_names <- names(low_contrib)
+  # for (i in seq_along(col_names)) {
+  #   full_match[, i] <- !is.na(match(data[, col_names[i]][[1]], low_contrib[, col_names[i]][[1]]))
+  # }
+  #
+  # # Replace resulting entries with "Rest"
+  # data[rowSums(full_match) == ncol(full_match), group_col] <- "Rest"
 
-  # Get species which always have a low contribution!
-  ts <- length(unique(data$time))
-  low_contrib <- data %>%
-    dplyr::filter_(~test <= combine_thresh) %>%
-    group_data(groups = c(groups[groups != "time"], group_col)) %>%
-    dplyr::summarise_(count = ~length(unique(time))) %>%
-    dplyr::filter_(~count == ts)
-  low_contrib$count <- NULL
-
-  # Get position of merge between data and low_contrib
-  full_match <- matrix(nrow = nrow(data), ncol = ncol(low_contrib))
-  col_names <- names(low_contrib)
-  for (i in seq_along(col_names)) {
-    full_match[, i] <- !is.na(match(data[, col_names[i]][[1]], low_contrib[, col_names[i]][[1]]))
-  }
-
-  # Replace resulting entries with "Rest"
-  data[rowSums(full_match) == ncol(full_match), group_col] <- "Rest"
+  # restrict number of potential grouping species to 15!
+  test <- agg_data(data, groups = c(groups[groups != "time"], group_col), fun = sum)
+  test <- dplyr::arrange_(as.data.frame(test), ~habitat, ~pred, ~desc(atoutput))
+  test <- group_data(test, groups = names(test)[names(test) != c("atoutput", )]
 
   # data[data$test <= combine_thresh, group_col] <- "Rest"
   data <- agg_data(data, groups = c(groups, group_col), fun = sum)
