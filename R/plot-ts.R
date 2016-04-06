70d44c37f962628bda20f852bdf7a16478299176
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-04-06 17:27:21 +0200

    well...

--- R/plot-ts.R
+++ R/plot-ts.R
@@ -23,23 +23,37 @@ plot_ts <- function(data, ex_data = NULL) {
   check_df_names(data = data, expect = c("time", "atoutput", "species"), optional = c("agecl", "run"))
 
   plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput))
+  # DOESNT WORK WITH ANNOTATE DUE TO FACET_WRAP!!!!!
   if (!is.null(ex_data)) {
     check_df_names(ex_data, expect = c("time", "atoutput", "species", "model"))
-    cols <- colorRampPalette(c(rgb(0,0,1,1), rgb(0,0,1,0)), alpha = TRUE)(10)
-    cols <- c(rev(cols), cols[-1])
-    wuwu <- ex_data
-    wuwu$time <- NULL
-    wuwu <- wuwu %>%
-      dplyr::group_by(species, model) %>%
-      dplyr::summarise(min = quantile(atoutput, probs = 0.25), max = quantile(atoutput, probs = 0.75))
-    wawa <- mapply(FUN = seq, wuwu$min, wuwu$max, MoreArgs = list(length.out = 19))
-    colnames(wawa) <- wuwu$species
-    wawa <- tidyr::gather_(as.data.frame(wawa), key = "species", value = "value", colnames(wawa))
-    wawa$col <- rep(cols, times = length(wuwu$species))
-    plot <- ggplot2::ggplot(data = wawa, ggplot2::aes_(yintercept = ~value, colour = ~value)) +
-      ggplot2::geom_hline()
+    models <- sort(unique(ex_data$model))
+    colors <- get_colpal()[1:length(unique(ex_data$model))]
+    for (i in seq_along(colors)) {
+      # create data for model!
+      wuwu <- ex_data
+      wuwu$time <- NULL
+      wuwu <- wuwu %>%
+        dplyr::filter_(~model == models[i]) %>%
+        dplyr::group_by_(~species) %>%
+        dplyr::summarise(min = quantile(atoutput, probs = 0.25), max = quantile(atoutput, probs = 0.75))
 
-    plot <- plot + ggplot2::scale_color_continuous()
+    }
+
+    # cols <- colorRampPalette(c(rgb(0,0,1,1), rgb(0,0,1,0)), alpha = TRUE)(10)
+    # cols <- c(rev(cols), cols[-1])
+    # wuwu <- ex_data
+    # wuwu$time <- NULL
+    # wuwu <- wuwu %>%
+    #   dplyr::group_by(species, model) %>%
+    #   dplyr::summarise(min = quantile(atoutput, probs = 0.25), max = quantile(atoutput, probs = 0.75))
+    # wawa <- mapply(FUN = seq, wuwu$min, wuwu$max, MoreArgs = list(length.out = 19))
+    # colnames(wawa) <- wuwu$species
+    # wawa <- tidyr::gather_(as.data.frame(wawa), key = "species", value = "value", colnames(wawa))
+    # wawa$col <- rep(cols, times = length(wuwu$species))
+    # plot <- ggplot2::ggplot(data = wawa, ggplot2::aes_(yintercept = ~value, colour = ~value)) +
+    #   ggplot2::geom_hline()
+    #
+    # plot <- plot + ggplot2::scale_color_continuous()
   }
   plot <- plot +
     ggplot2::geom_line() +
