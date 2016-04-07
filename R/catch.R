fff388f76cd733399fac2839d85b874d76366f50
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-04-07 14:25:00 +0200

    Draft for catch data load procedure.
    Repo should brake due to this.

---
+++ R/catch.R
@@ -0,0 +1,20 @@
+setwd("c:/backup_c/ATLANTIS_Stuff/Baseruns/1235_v.13.0.0_ATLANTIS_NS/")
+catch <- load_nc(nc = "outputNorthSeaCATCH.nc", fgs = "functionalGroups.csv", bps = load_bps(fgs = "functionalGroups.csv", init = "init_NorthSea.nc"), select_groups = get_groups(fgs = "functionalGroups.csv"), select_variable = "Catch", bboxes = get_boundary(load_box(bgm = "NorthSea.bgm")))
+
+at_out <- RNetCDF::open.nc("outputNorthSeaCATCH.nc")
+
+var_names_ncdf <- sapply(seq_len(RNetCDF::file.inq.nc(at_out)$nvars - 1),
+                         function(x) RNetCDF::var.inq.nc(at_out, x)$name)
+#
+n_timesteps <- RNetCDF::dim.inq.nc(at_out, 0)$length
+n_boxes     <- RNetCDF::dim.inq.nc(at_out, 1)$length
+n_layers    <- RNetCDF::dim.inq.nc(at_out, 2)$length
+
+cod1 <- RNetCDF::var.get.nc(ncfile = at_out, variable = "cod1_Catch")
+cod2 <- RNetCDF::var.get.nc(ncfile = at_out, variable = "cod2_Catch")
+
+lapply(catch[, 1:ncol(catch)-1], unique)
+
+
+
+
