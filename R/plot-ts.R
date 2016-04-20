8e9c547de5def03764ebc7f97ec9848f23e00701
alketh <Alexander.Keth@uni-hamburg.de>
2016-04-20 13:38:42 +0200

    Generic plot_ts.

--- R/plot-ts.R
+++ R/plot-ts.R
@@ -8,14 +8,14 @@
 #' @examples
 #' plot_ts(preprocess_setas$biomass_age)
 
-plot_ts <- function(data) {
+plot_ts <- function(data, x = "time", y = "atoutput", wrap = "species") {
   if (!any(is.element(names(data), "time"))) {
     stop("Column time not found in data")
   }
 
-  plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = ~time, y = ~atoutput)) +
+  plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = lazyeval::interp(~var, var = as.name(x)), y = lazyeval::interp(~var, var = as.name(y)))) +
     ggplot2::geom_line() +
-    ggplot2::facet_wrap(~species, scales = "free_y", ncol = 9, labeller = ggplot2::label_wrap_gen(width = 15)) +
+    ggplot2::facet_wrap(lazyeval::interp(~var, var = as.name(wrap)), scales = "free_y", ncol = 9, labeller = ggplot2::label_wrap_gen(width = 15)) +
     ggplot2::guides(col = ggplot2::guide_legend(nrow = 1)) +
     theme_atlantis()
   plot <- ggplot_custom(plot)
