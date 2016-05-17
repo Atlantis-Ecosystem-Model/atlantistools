26b640ee7479b940813ebe5c9cbb336265e4ae9b
alketh <Alexander.Keth@uni-hamburg.de>
2016-05-17 15:58:21 +0200

    Add test for load_dietmatrix.

---
+++ tests/testthat/test-load-dietmatrix.R
@@ -0,0 +1,11 @@
+context("load-dietmatrix test output")
+
+dm <- load_dietmatrix(dir = system.file("extdata", "setas-model-new-becdev", package = "atlantistools"),
+                      prm_biol = "VMPA_setas_biol_fishing_New.prm",
+                      fgs = "SETasGroups.csv")
+
+test_that("test output numbers", {
+  expext_equal(length(unique(dm$code)), 159)
+  expect_equal(dm$avail[dm$pred == "FVS" & dm$prey_stanza == 1 & dm$pred_stanza == 2 & dm$prey == "FVV"], 0.02)
+  expect_equal(dm$avail[dm$pred == "PWN" & dm$pred_stanza == 2 & dm$prey == "PL"], 0.18)
+})
