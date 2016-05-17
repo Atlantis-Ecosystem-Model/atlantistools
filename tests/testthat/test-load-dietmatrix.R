e6976c75f510d7a741f5c69960c360098564e015
alketh <Alexander.Keth@uni-hamburg.de>
2016-05-17 16:16:53 +0200

    Fix typo.

--- tests/testthat/test-load-dietmatrix.R
+++ tests/testthat/test-load-dietmatrix.R
@@ -5,7 +5,7 @@ dm <- load_dietmatrix(dir = system.file("extdata", "setas-model-new-becdev", pac
                       fgs = "SETasGroups.csv")
 
 test_that("test output numbers", {
-  expext_equal(length(unique(dm$code)), 159)
+  expect_equal(length(unique(dm$code)), 159)
   expect_equal(dm$avail[dm$pred == "FVS" & dm$prey_stanza == 1 & dm$pred_stanza == 2 & dm$prey == "FVV"], 0.02)
   expect_equal(dm$avail[dm$pred == "PWN" & dm$pred_stanza == 2 & dm$prey == "PL"], 0.18)
 })
