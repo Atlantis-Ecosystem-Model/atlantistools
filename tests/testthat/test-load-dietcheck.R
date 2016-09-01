5639c936821b5cede627e8dba20404840c3e71f9
alketh <Alexander.Keth@uni-hamburg.de>
2016-09-01 14:18:12 +0200

    Add tests to load_dietcheck.
    Tests are quite verbose. This may result in test failure as soon as the txt files are updated. Nonetheless, it should be straight forward to update the values. copied the results directly from the txt files.

--- tests/testthat/test-load-dietcheck.R
+++ tests/testthat/test-load-dietcheck.R
@@ -15,4 +15,20 @@ test_that("test output numbers", {
   expect_equal(dim(diet), c(390, 5))
   expect_is(diet$pred, "character")
   expect_is(diet$prey, "character")
+  expect_equal(diet$atoutput[diet$pred == "CEP" & diet$time == 240 & diet$habitat == "WC" & diet$prey == "FPS"], 0.001663077)
+  expect_equal(diet$atoutput[diet$pred == "ZM" & diet$time == 480 & diet$habitat == "WC" & diet$prey == "DL"], 9.23446e-13)
+
+})
+
+
+diet <- load_dietcheck(system.file("extdata", "setas-model-new-trunk", package = "atlantistools"),
+                       dietcheck = "outputSETASDietCheck.txt", report = FALSE, version_flag = 2)
+
+test_that("test output numbers trunk", {
+  # expect_true(all(abs(test1$check - 1) < 0.001))
+  expect_equal(dim(diet), c(3722, 5))
+  expect_is(diet$pred, "character")
+  expect_is(diet$prey, "character")
+  expect_equal(diet$atoutput[diet$pred == "FVS" & diet$time == 570 & diet$agecl == 3 & diet$prey == "FVS"], 0.1192798)
+  expect_equal(diet$atoutput[diet$pred == "FPS" & diet$time == 720 & diet$agecl == 6 & diet$prey == "DR"], 0.001334405)
 })
