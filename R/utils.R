f1cdb52a37367a7f45d9638790688addbbe03e82
alketh <Alexander.Keth@uni-hamburg.de>
2016-09-09 16:23:43 +0200

    Update variable creation for ns debugging.

--- R/utils.R
+++ R/utils.R
@@ -2,7 +2,8 @@
 # Health care warning: Heavy use of the assign function...
 # Variables are created in the GlobalEnvironment...
 debug_ns <- function() {
-  assign("dir", "z:/Atlantis_models/Runs/dummy_01_ATLANTIS_NS/", envir = .GlobalEnv)
+  d <- "z:/Atlantis_models/Runs/dummy_01_ATLANTIS_NS/"
+  assign("dir", d, envir = .GlobalEnv)
   assign("fgs", "functionalGroups.csv", envir = .GlobalEnv)
   assign("nc", "outputNorthSea.nc", envir = .GlobalEnv)
   assign("nc_gen", "outputNorthSea.nc", envir = .GlobalEnv)
@@ -14,10 +15,10 @@ debug_ns <- function() {
   assign("specpredmort", "outputNorthSeaSpecificPredMort.txt", envir = .GlobalEnv)
   assign("prm_biol", "NorthSea_biol_fishing.prm", envir = .GlobalEnv)
   assign("prm_run", "NorthSea_run_fishing_F.prm", envir = .GlobalEnv)
-  assign("bps", load_bps(fgs = "functionalGroups.csv", init = "init_NorthSea.nc"), envir = .GlobalEnv)
+  assign("bps", load_bps(dir = d, fgs = "functionalGroups.csv", init = "init_NorthSea.nc"), envir = .GlobalEnv)
   assign("fgs", "functionalGroups.csv", envir = .GlobalEnv)
-  assign("select_groups", get_groups(fgs = "functionalGroups.csv"), envir = .GlobalEnv)
-  assign("bboxes", get_boundary(load_box(bgm = "NorthSea.bgm")), envir = .GlobalEnv)
+  assign("select_groups", get_groups(dir = d, fgs = "functionalGroups.csv"), envir = .GlobalEnv)
+  assign("bboxes", get_boundary(load_box(dir = d, bgm = "NorthSea.bgm")), envir = .GlobalEnv)
   assign("out", "preprocess-north-sea.rda", envir = .GlobalEnv)
   assign("save_to_disc", FALSE, envir = .GlobalEnv)
   assign("check_acronyms", TRUE, envir = .GlobalEnv)
