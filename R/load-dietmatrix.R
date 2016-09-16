c524b9b154d3a6df35e9097d416e204b1d3fe4e5
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-16 14:58:41 +1000

    Moce write_avail to load_dietmatrix.

--- R/load-dietmatrix.R
+++ R/load-dietmatrix.R
@@ -18,6 +18,9 @@
 #' "long" (\code{transform = TRUE, default}) or "wide" (\code{transform = FALSE})
 #' format. You should use the "wide" format in case you aim to change your
 #' diet matrix entries.
+#' @param dietmatrix Dataframe in 'long' format containing information about availabilities
+#' with columns 'pred', 'prey', 'pred_stanza', 'prey_stanza', 'code', 'prey_id' and
+#' 'avail'. The dataframe should be generated with \code{load_dietmatrix()}.
 #' @return dataframe of the availability matrix in long format with columns
 #' pred, pred_stanza (1 = juvenile, 2 = adult), prey_stanza, prey, avail, code.
 #' @export
@@ -85,6 +88,50 @@ load_dietmatrix <- function(dir = getwd(), prm_biol, fgs, transform = TRUE) {
   return(result)
 }
 
+
+#' @export
+#' @rdname change_avail
+# Write dietmatrix dataframe in wide format to hdd.
+write_diet <- function(dir = getwd(), dietmatrix, prm_biol) {
+  # Find dietmatrix in biological parameterfile!
+  pstring <- "pPREY"
+  biol <- convert_path(dir = dir, file = prm_biol)
+  biol <- readLines(biol)
+  pos <- grep(pattern = pstring, x = biol)
+
+  # Remove explanatory rows
+  pos <- pos[pos != vapply(c("pPREY1FY1", "pPREY1FY2"), FUN = grep, FUN.VALUE = integer(1), x = biol)]
+
+  # Define start and end of dietmatrix
+  lags <- diff(pos)
+  if (any(lags > 3)) {
+    # Some models overwrite invertebrate availabilities.
+    warning(paste0("Multiple linesbreaks present in ", prm_biol,
+                   ". End of dietmatrix might not be detected correctly. Please check your parameter file."))
+    dm_ids <- min(pos) : (pos[which(lags >= 4)] + 1)
+  } else {
+    dm_ids <- min(pos) : (max(pos) + 1)
+  }
+
+  # Write dietmatrix to prm_biol
+  if (length(dm_ids) < (2 * nrow(dietmatrix))) {
+    stop(paste0("Dietmatrix does not fit into ", prm_biol, ". Parameters are lost!"))
+  } else {
+    breaks <- length(dm_ids) - 2 * nrow(dietmatrix)
+    avails <- apply(X = dietmatrix[, 5:ncol(dietmatrix)], FUN = paste, MARGIN = 1, collapse = "\t")
+    tags <- paste(dietmatrix[, "code"], ncol(dietmatrix) - 4, sep = "\t")
+    dm_paste <- unlist(Map(f = c, tags, avails, USE.NAMES = F))
+    dm_paste <- c(dm_paste, rep("", times = breaks))
+
+    if (length(dm_paste) == length(dm_ids)) {
+      biol[dm_ids] <- dm_paste
+      print("Writing new prm file!")
+      writeLines(biol, convert_path(dir = dir, file = prm_biol))
+    } else {
+      stop("Dimensions do not match. Dietmatrix not updated!")
+    }
+  }
+}
 
 
 
