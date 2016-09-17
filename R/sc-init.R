2f6e749f6654ce9c1c1f05bf0d5b1b69400166dc
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-17 20:28:19 +1000

    Further improve sc_init.

--- R/sc-init.R
+++ R/sc-init.R
@@ -68,18 +68,31 @@ sc_init <- function(dir = getwd(), nc, init, prm_biol, fgs, bboxes) {
 
   # Extract data for age based groups
   weights <- load_init_weight(dir = dir, nc = init, fgs = fgs)
-  pd <- lapply(acr_age, get_pred_data, dir = dir, prm_biol = prm_biol)
+  pd <- do.call(rbind, lapply(acr_age, get_pred_data, dir = dir, prm_biol = prm_biol)) %>%
+    dplyr::left_join(data.frame(acronym = acr_age, species = groups_age, stringsAsFactors = FALSE)) %>%
+    dplyr::select(-acronym) %>%
+    dplyr::left_join(weights)
   nums <- load_nc(dir = dir, nc = nc, bps = bps, select_variable = "Nums", fgs = fgs, select_groups = groups_age, bboxes = bboxes) %>%
     dplyr::filter(time == 0)
 
-  # Convert numbers to biomass density!
+  get_vert_distrib <- function(dir, predacr, prm_biol) {
+    tags <- paste("VERTday", as.vector(outer(X = predarc, Y = 1:2, FUN = paste0)), sep = "_")
+    sapply(tags, extract_prm, dir = dir, prm_biol = prm_biol, min_only = FALSE)
+    # VERTday_BWH1	7
+
+
+  }
+
+  # Convert numbers to biomass density! --> distribute over watercolumn!
   dens <- dplyr::left_join(nums, weights) %>%
     dplyr::left_join(vol) %>%
-    dplyr::mutate(atoutput = (rn + sn) * atoutput / vol)
+    dplyr::mutate(atoutput = (rn + sn) * atoutput / vol) %>%
+    dplyr::select(-rn, -sn, -vol)
 
-  # Get nitrogen desity for non age based groups
+  # Get nitrogen desity for non age based groups and combine with age based data
   n <- load_nc(dir = dir, nc = nc, bps = bps, fgs = fgs, select_groups = groups_rest, select_variable = "N", bboxes = bboxes) %>%
-    dplyr::filter(time == 0)
+    dplyr::filter(time == 0) %>%
+    rbind(dens)
 
   # Extract availability matrix and split into predator groups!
   dm <- load_dietmatrix(dir = dir, prm_biol = prm_biol, fgs = fgs) %>%
