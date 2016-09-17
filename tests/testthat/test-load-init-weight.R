951f9868fedf341ce4319b166cfa260028c84191
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-17 20:27:01 +1000

    Update test according to new init file.

--- tests/testthat/test-load-init-weight.R
+++ tests/testthat/test-load-init-weight.R
@@ -2,11 +2,10 @@ context("load_init_weight check structure and values in output dataframe")
 
 d <- system.file("extdata", "gns", package = "atlantistools")
 
-iw <- load_init_weight(dir = d, nc = "init_NorthSea.nc", fgs = "functionalGroups.csv")
+iw <- load_init_weight(dir = d, nc = "init_simple_NorthSea.nc", fgs = "functionalGroups.csv")
 
-test_that("test output numbers", {
-  expect_equal(dim(iw), c(226, 4))
+test_that("test output numbers", {iw
+  expect_equal(dim(iw), c(24, 4))
   expect_equal(iw$rn[iw$species == "cod" & iw$agecl == 7], 61272.89)
-  expect_equal(iw$sn[iw$species == "whiting" & iw$agecl == 3], 768.94)
   expect_equal(iw$sn[iw$species == "herring" & iw$agecl == 9], 718.66)
 })
