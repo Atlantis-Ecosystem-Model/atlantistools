3f4304af239ba9641f9cd8dec11c8638082c777a
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-17 17:03:58 +1000

    1st draft for sanity check.

---
+++ R/sc-init.R
@@ -0,0 +1,70 @@
+#' Sanity check initial conditions file
+#'
+#' @param dir Character string giving the path of the Atlantis model folder.
+#' If data is stored in multiple folders (e.g. main model folder and output
+#' folder) you should use 'NULL' as dir.
+#' @param nc Character string giving the filename of the general netcdf
+#' file Usually "output[...].nc". In case you are using
+#' multiple folders for your model files and outputfiles pass the complete
+#' folder/filename string as nc_gen. In addition set dir to 'NULL' in this
+#' case.
+#' @param init Character string giving the filename of the initial conditions netcdf
+#' file Usually "init[...].nc".
+#' @param prm_biol Character string giving the filename of the biological
+#' parameterfile. Usually "[...]biol_fishing[...].prm". In case you are using
+#' multiple folders for your model files and outputfiles pass the complete
+#' folder/filename string and set dir to 'NULL'.
+#' @param fgs Character string giving the filename of 'functionalGroups.csv'
+#' file. In case you are using multiple folders for your model files and
+#' outputfiles pass the complete folder/filename string as fgs.
+#' In addition set dir to 'NULL' in this case.
+#' @param bboxes Integer vector giving the box-id of the boundary boxes.
+#' @return Named list with the dataframes as list entry saved as .Rda file.
+#'
+#' @examples
+
+#' @export
+
+# dir <- base_path
+# fgs <- "functionalGroups.csv"
+# init <- "init_NorthSea.nc"
+# nc <- "outputNorthSea.nc"
+# prm_biol <- "NorthSea_biol_fishing.prm"
+# bboxes <- get_boundary(load_box(bgm = "NorthSea.bgm"))
+
+# function start
+sc_init <- function(dir = getwd(), nc, init, prm_biol, fgs, bboxes) {
+  acr_age <- get_age_acronyms(dir = dir, fgs = fgs)
+  bps <- load_bps(fgs = fgs, init = init)
+
+  groups <- get_groups(dir = dir, fgs = fgs)
+  groups_age <- get_age_groups(dir = dir, fgs = fgs)
+  groups_rest <- groups[!is.element(groups, groups_age)]
+
+  get_pred_data <- function(dir, prm_biol, predacr) {
+    mum <- extract_prm_cohort(dir = dir, prm_biol = prm_biol, variables = paste0("mum_", predacr))[1, ]
+    c <- extract_prm_cohort(dir = dir, prm_biol = prm_biol, variables = paste0("C_", predacr))[1, ]
+    prms <- vapply(paste0(c("E", "EPlant", "EDL", "EDR", "KWSR", "KWRR"), "_", predacr),
+                   extract_prm, dir = getwd(), prm_biol = "NorthSea_biol_fishing.prm", numeric(1), USE.NAMES = FALSE)
+    prms <- c(prms, extract_prm(prm_biol = "NorthSea_biol_fishing.prm", variable = paste0(predacr, "_AgeClassSize")))
+
+    df <- data.frame(acronym = predacr, mum = mum, c = c, stringsAsFactors = FALSE)
+    df$agecl <- 1:nrow(df)
+    df <- cbind(df, sapply(prms, rep, each = nrow(df)))
+    names(df)[5:ncol(df)] <- c("e", "eplant", "edl", "edr", "kwrr", "kwsr", "acs")
+
+    return(df)
+  }
+
+  weights <- load_init_weight(nc = "init_NorthSea.nc", fgs = "functionalGroups.csv")
+
+  pd <- lapply(acr_age, get_pred_data, dir = dir, prm_biol = prm_biol)
+
+  nums <- load_nc(dir = dir, nc = nc, bps = bps, select_variable = "Nums", fgs = fgs,
+                  select_groups = groups_age, bboxes = bboxes) %>%
+    filter(time == 0)
+
+  n <- load_nc(dir = dir, nc = nc, bps = bps, fgs = fgs, select_groups = groups_rest,
+               select_variable = "N", bboxes = bboxes) %>%
+    filter(time == 0)
+}
