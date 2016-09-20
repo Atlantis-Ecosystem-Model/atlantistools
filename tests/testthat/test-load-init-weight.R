98c7b25129881964c45395e5192de0fd6382bbb7
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-20 23:51:38 +1000

    Test extraction of layer numbers per box.

--- tests/testthat/test-load-init-weight.R
+++ tests/testthat/test-load-init-weight.R
@@ -4,7 +4,7 @@ d <- system.file("extdata", "gns", package = "atlantistools")
 
 iw <- load_init_weight(dir = d, init = "init_simple_NorthSea.nc", fgs = "functionalGroups.csv")
 
-test_that("test output numbers", {iw
+test_that("test output numbers", {
   expect_equal(dim(iw), c(24, 4))
   expect_equal(iw$rn[iw$species == "cod" & iw$agecl == 7], 61272.89)
   expect_equal(iw$sn[iw$species == "herring" & iw$agecl == 9], 718.66)
@@ -12,4 +12,14 @@ test_that("test output numbers", {iw
 
 num <- load_init_num(dir = d, init = "init_simple_NorthSea.nc", fgs = "functionalGroups.csv")
 n <- load_init_n(dir = d, init = "init_simple_NorthSea.nc", fgs = "functionalGroups.csv")
+
+layers <- get_layers(dir = d, nc = "init_simple_NorthSea.nc")
+
+test_that("test get_layers", {
+  expect_equal(length(layers), 26)
+  expect_equal(max(layers), 7)
+  expect_equal(iw$sn[iw$species == "herring" & iw$agecl == 9], 718.66)
+})
+
+
 
