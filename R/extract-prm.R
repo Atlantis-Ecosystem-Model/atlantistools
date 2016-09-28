09a0cf33afcb7bed2fdce22311b8ae4d8d23c172
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-09-28 14:20:14 +1000

    Add function to read in age specific parameters.

--- R/extract-prm.R
+++ R/extract-prm.R
@@ -53,15 +53,12 @@ extract_prm_cohort <- function(dir = getwd(), prm_biol, variables) {
 }
 
 
-prm_to_df <- function(dir = getwd(), prm_biol, fgs, group, parameter) {
-  set_single_prm <- function(group, parameter) {
-    if (parameter %in% c("AgeClassSize", "age_mat")) {
-      paste(group, parameter, sep = "_")
-    } else {
-      paste(parameter, group, sep = "_")
-    }
-  }
+# dir <- "c:/backup_z/Atlantis_models/baserun/"
+# prm_biol <- "NorthSea_biol_fishing.prm"
+# group <- c("COD", "WHG")
+# parameter <- c("mum", "C")
 
+prm_to_df <- function(dir = getwd(), prm_biol, fgs, group, parameter) {
   # Extract data!
   prms <- lapply(parameter, set_single_prm, group = group)
   values <- lapply(prms, extract_prm, dir = dir, prm_biol = prm_biol)
@@ -71,34 +68,42 @@ prm_to_df <- function(dir = getwd(), prm_biol, fgs, group, parameter) {
   names(df) <- tolower(parameter)
   df$species <- group
   df$species <- convert_factor(data_fgs = load_fgs(dir = dir, fgs = fgs), col = df$species)
+  df <- dplyr::select_(df, .dots = c("species", sort(names(df)[-ncol(df)])))
 
   return(df)
 }
 
 
-prm_to_df <- function(dir = getwd(), prm_biol, fgs, group, parameter) {
-  set_single_prm <- function(group, parameter) {
-    if (parameter %in% c("AgeClassSize", "age_mat")) {
-      paste(group, parameter, sep = "_")
-    } else {
-      paste(parameter, group, sep = "_")
-    }
-  }
-
+prm_to_df_ages <- function(dir = getwd(), prm_biol, fgs, group, parameter) {
   # Extract data!
   prms <- lapply(parameter, set_single_prm, group = group)
-  values <- lapply(prms, extract_prm, dir = dir, prm_biol = prm_biol)
+  values <- lapply(prms, extract_prm_cohort, dir = dir, prm_biol = prm_biol)
 
   # Combine to df!
-  df <- as.data.frame(do.call(cbind, values))
-  names(df) <- tolower(parameter)
-  df$species <- group
-  df$species <- convert_factor(data_fgs = load_fgs(dir = dir, fgs = fgs), col = df$species)
+  nc <- sapply(values, function(x) sapply(x, length), USE.NAMES = FALSE)[, 1]
+  df <- data.frame(values = unlist(values))
+  df$species <- rep(unlist(Map(rep, group, nc)), times = length(parameter))
+  df$agecl <- rep(unlist(sapply(nc, seq, from = 1)), times = length(parameter))
+  df$prm <- rep(tolower(parameter), each = sum(nc))
 
-  return(df)
+  result <- tidyr::spread_(data = df, key_col = "prm", value_col = "values")
+
+  result$species <- convert_factor(data_fgs = load_fgs(dir = dir, fgs = fgs), col = result$species)
+
+  return(result)
 }
 
 
+# helper function to create an Atlantis parameter Flag given a vector of groups and
+# a vector of parameters. You can pass different numbers of groups and parameters.
+# In addition
+set_single_prm <- function(group, parameter) {
+  if (parameter %in% c("AgeClassSize", "age_mat")) {
+    paste(group, parameter, sep = "_")
+  } else {
+    paste(parameter, group, sep = "_")
+  }
+}
 
 
 
