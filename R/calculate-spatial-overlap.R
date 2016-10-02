9bd1583a399ed12b80d7be9d6ebc4b96f1e7eea9
Alexander Keth <alexander.keth@uni-hamburg.de>
2016-10-02 15:44:31 +1100

    Update calculate_spatial_overlap.

--- R/calculate-spatial-overlap.R
+++ R/calculate-spatial-overlap.R
@@ -38,45 +38,28 @@ calculate_spatial_overlap <- function(biomass_spatial, dietmatrix, agemat) {
 
   # Step1: Calculate relative biomass per box and layer per group and stanza!
   # - Age based groups!
-  names(data_bio$structn)[names(data_bio$structn) == "atoutput"] <- "sn"
-  names(data_bio$resn)[names(data_bio$resn) == "atoutput"] <- "rn"
-  biomass_age <- dplyr::inner_join(data_bio$nums, data_bio$structn) %>%
-    dplyr::left_join(data_bio$resn) %>%
-    dplyr::mutate_(.dots = stats::setNames(list(~convert_factor(data_fgs = fgs_data, col = species)), "species")) %>%
-    dplyr::left_join(age_mat) %>%
-    dplyr::mutate_(.dots = stats::setNames(list(~ifelse(agecl < age_mat, 1, 2)), "pred_stanza")) %>%
-    dplyr::mutate_(.dots = stats::setNames(list(~(sn + rn) * atoutput * bio_conv), "bio")) %>%
-    agg_data(col = "bio", groups = c("species", "pred_stanza", "time", "polygon", "layer"), out = "bio", fun = sum)
-
-  # - Non age based groups!
-  vol <- tidyr::spread_(data = vol, key_col = c("variable"), value_col = "atoutput")
-  biomass_pools <- dplyr::left_join(data_bio$n, vol)
-  biomass_pools$bio <- with(biomass_pools, ifelse(species %in% bps, atoutput * volume / dz * bio_conv, atoutput * volume * bio_conv))
-  biomass_pools <- dplyr::select_(biomass_pools, .dots = c("species", "time", "polygon", "layer", "bio"))
-  biomass_pools$pred_stanza <- 1
-  biomass_pools$species <- convert_factor(data_fgs = fgs_data, col = biomass_pools$species)
-
-  # Combine with biomass from age-groups and calculate % biomass per box and layer!
-  biomass <- dplyr::bind_rows(biomass_age, biomass_pools) %>%
-    agg_perc(col = "bio", groups = c("species", "time", "pred_stanza"), out = "perc_bio")
+  bio_stanza <- combine_ages(biomass_spatial, grp_col = "species", agemat = agemat)
+  bio_stanza$species_stanza[is.na(bio_stanza$species_stanza)] <- 1 # Not sure if this is correct!
+  biomass <- agg_perc(bio_stanza, groups = c("time", "species", "species_stanza"), out = "perc_bio")
 
   # Fill data gaps to make sure that both combinations are present later:
   # - pred/stanza present in box/layer combination & prey/stanza absent
   # - prey/stanza present in box/layer combination $ pred/stanza absent
   ac_box_layer <- unique(dplyr::select_(dplyr::ungroup(biomass), .dots = c("polygon", "layer")))
-  ac_pred_stan <- unique(dplyr::select_(dplyr::ungroup(biomass), .dots = c("species", "pred_stanza")))
+  ac_pred_stan <- unique(dplyr::select_(dplyr::ungroup(biomass), .dots = c("species", "species_stanza")))
   ac_time <- unique(dplyr::select_(dplyr::ungroup(biomass), .dots = c("time")))
 
-  data_bio <- merge(ac_box_layer, ac_pred_stan)
-  data_bio <- merge(data_bio, ac_time) %>%
-    dplyr::left_join(biomass)
+  data_bio <- merge(ac_box_layer, ac_pred_stan) %>%
+    merge(ac_time) %>%
+    dplyr::left_join(biomass, by = c("time", "species", "species_stanza", "polygon", "layer"))
   data_bio$perc_bio[is.na(data_bio$perc_bio)] <- 0
 
-
-  # Apply calculations to all predators!
-  preds <- rep(convert_factor(data_fgs = fgs_data, col = acr_age), each = 2)
-  stanzas <- rep(1:2, times = length(acr_age))
-  sis <- Map(schoener, pred = preds, pred_stanza = stanzas, MoreArgs = list(biomass = data_bio, avail = data_dm))
+  # Apply Schoener calculations to all predators!
+  ps <- data_bio %>%
+    dplyr::select_(.dots = c("species", "species_stanza")) %>%
+    unique()
+  sis <- Map(schoener, pred = ps$species, pred_stanza = ps$species_stanza,
+             MoreArgs = list(biomass = data_bio, avail = dietmatrix))
 }
 
 
@@ -84,25 +67,29 @@ calculate_spatial_overlap <- function(biomass_spatial, dietmatrix, agemat) {
 # - pred: %biomass per predator ageclass per time, box, layer
 # - avail: availability matrix
 # - prey: overall preybiomass
-# pred <- "Cod"
-# pred_stanza <- 1
-# avail <- dm
+# pred <- ps$species[1]
+# pred_stanza <- ps$species_stanza[1]
+# biomass <- data_bio
+# avail <- dietmatrix
 schoener <- function(pred, pred_stanza, biomass, avail) {
   if (!(length(pred) == 1 & length(pred_stanza) == 1)) {
     stop("Only one predator/agecl combination allowed in dataframe 'pred'.")
   }
 
-  df_pred <- biomass[biomass$pred_stanza == pred_stanza & biomass$species == pred, ]
-
+  # Select specific predator/predator stanza combination! (columns still called species at this point)
+  df_pred <- biomass[biomass$species_stanza == pred_stanza & biomass$species == pred, ]
   df_avail <- avail[avail$pred == pred & avail$pred_stanza == pred_stanza, ]
+  # Remove predator/predstanza since overlap is 1 by default!
+  biomass_clean <- dplyr::anti_join(biomass, df_pred)
+  # Remove sediment layer! We need to read in
 
   # Combine predator data with prey data!
   # WARNING: This may lead to a very huge dataframe... all (even non existing)
-  # pred/prey combinations are combined!
+  # pred/prey combinations are combined! Therefore the calculations are applied per predator.
   # The 2nd join has to be an inner_join to make sure only available prey groups are
   # used to calculate the overlap index!
-  si <- dplyr::left_join(df_avail, df_pred, by = c("pred" = "species", "pred_stanza")) %>%
-    dplyr::inner_join(biomass, by = c("prey" = "species", "prey_stanza" = "pred_stanza", "time", "polygon", "layer"))
+  si <- dplyr::left_join(df_avail, df_pred, by = c("pred" = "species", "pred_stanza" = "species_stanza")) %>%
+    dplyr::inner_join(biomass, by = c("prey" = "species", "prey_stanza" = "species_stanza", "time", "polygon", "layer"))
 
   si$si <- with(si, abs(perc_bio.x - perc_bio.y))
 
@@ -119,11 +106,11 @@ schoener <- function(pred, pred_stanza, biomass, avail) {
     agg_data(col = "si", groups = c("time", "pred", "pred_stanza"), out = "si", fun = sum)
 
   return(list(si_spec, si_overall))
-  # ggplot2::ggplot(si_spec, ggplot2::aes(x = time, y = si, group = time)) +
-  #   ggplot2::geom_violin() +
-  #   ggplot2::geom_point(data = si_overall, colour = "red")
 }
 
+ggplot2::ggplot(si_spec, ggplot2::aes(x = time, y = si, group = time)) +
+  ggplot2::geom_violin() +
+  ggplot2::geom_point(data = si_overall, colour = "red")
 
 # ggplot2::ggplot(sis[[18]][[1]], ggplot2::aes(x = time, y = si, group = time)) +
 #   ggplot2::geom_violin() +
