46937e2c0dd1bdb17e664f994380076aee568545
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-12 17:01:49 +0200

    Use setas_trunk files as reference dataframes.

--- data-raw/data-preprocess.R
+++ data-raw/data-preprocess.R
@@ -1,7 +1,7 @@
-d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
+d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
 
 boundary_boxes <- get_boundary(boxinfo = load_box(dir = d, bgm = "VMPA_setas.bgm"))
-epibenthic_groups <- load_bps(dir = d, fgs = "SETasGroups.csv", init = "init_vmpa_setas_25032013.nc")
+epibenthic_groups <- load_bps(dir = d, fgs = "SETasGroupsDem_NoCep.csv", init = "INIT_VMPA_Jan2015.nc")
 groups <- c("Planktiv_S_Fish", "Pisciv_S_Fish", "Cephalopod", "Megazoobenthos", "Diatom", "Zoo", "Lab_Det", "Ref_Det")
 groups_age <- groups[1:2]
 groups_rest <- groups[3:length(groups)]
@@ -9,10 +9,10 @@ groups_rest <- groups[3:length(groups)]
 ref_eat <- load_nc(dir = d,
                    nc = "outputSETASPROD.nc",
                    bps = epibenthic_groups,
-                   fgs = "SETasGroups.csv",
+                   fgs = "SETasGroupsDem_NoCep.csv",
                    select_groups = groups_age,
                    select_variable = "Eat",
-                   prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                   prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                    bboxes = boundary_boxes,
                    check_acronyms = TRUE)
 
@@ -22,10 +22,10 @@ devtools::use_data(ref_eat, overwrite = TRUE)
 ref_grazing <- load_nc(dir = d,
                        nc = "outputSETASPROD.nc",
                        bps = epibenthic_groups,
-                       fgs = "SETasGroups.csv",
+                       fgs = "SETasGroupsDem_NoCep.csv",
                        select_groups = groups_rest,
                        select_variable = "Grazing",
-                       prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                       prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                        bboxes = boundary_boxes,
                        check_acronyms = TRUE)
 
@@ -35,10 +35,10 @@ devtools::use_data(ref_grazing, overwrite = TRUE)
 ref_n <- load_nc(dir = d,
                  nc = "outputSETAS.nc",
                  bps = epibenthic_groups,
-                 fgs = "SETasGroups.csv",
+                 fgs = "SETasGroupsDem_NoCep.csv",
                  select_groups = groups_rest,
                  select_variable = "N",
-                 prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                 prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                  bboxes = boundary_boxes,
                  check_acronyms = TRUE)
 
@@ -48,10 +48,10 @@ devtools::use_data(ref_n, overwrite = TRUE)
 ref_nums <- load_nc(dir = d,
                     nc = "outputSETAS.nc",
                     bps = epibenthic_groups,
-                    fgs = "SETasGroups.csv",
+                    fgs = "SETasGroupsDem_NoCep.csv",
                     select_groups = groups_age,
                     select_variable = "Nums",
-                    prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                    prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                     bboxes = boundary_boxes,
                     check_acronyms = TRUE)
 
@@ -60,10 +60,10 @@ devtools::use_data(ref_nums, overwrite = TRUE)
 ref_structn <- load_nc(dir = d,
                        nc = "outputSETAS.nc",
                        bps = epibenthic_groups,
-                       fgs = "SETasGroups.csv",
+                       fgs = "SETasGroupsDem_NoCep.csv",
                        select_groups = groups_age,
                        select_variable = "StructN",
-                       prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                       prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                        bboxes = boundary_boxes,
                        check_acronyms = TRUE)
 
@@ -72,14 +72,19 @@ devtools::use_data(ref_structn, overwrite = TRUE)
 ref_resn <- load_nc(dir = d,
                     nc = "outputSETAS.nc",
                     bps = epibenthic_groups,
-                    fgs = "SETasGroups.csv",
+                    fgs = "SETasGroupsDem_NoCep.csv",
                     select_groups = groups_age,
                     select_variable = "ResN",
-                    prm_run = "VMPA_setas_run_fishing_F_New.prm",
+                    prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
                     bboxes = boundary_boxes,
                     check_acronyms = TRUE)
 
 devtools::use_data(ref_resn, overwrite = TRUE)
+
+ref_vol <- load_nc_physics(dir = d, nc = "outputSETAS.nc", select_physics = c("volume", "dz"),
+                           prm_run = "VMPA_setas_run_fishing_F_Trunk.prm", bboxes = boundary_boxes, aggregate_layers = F)
+
+devtools::use_data(ref_vol, overwrite = TRUE)
 
 preprocess_setas <- preprocess(dir = d,
    nc_gen = "outputSETAS.nc",
@@ -89,10 +94,10 @@ preprocess_setas <- preprocess(dir = d,
    ssb = "outputSETASSSB.txt",
    specmort = "outputSETASSpecificMort.txt",
    specpredmort = "outputSETASSpecificPredMort.txt",
-   prm_biol = "VMPA_setas_biol_fishing_New.prm",
-   prm_run = "VMPA_setas_run_fishing_F_New.prm",
+   prm_biol = "VMPA_setas_biol_fishing_Trunk.prm",
+   prm_run = "VMPA_setas_run_fishing_F_Trunk.prm",
    bps = epibenthic_groups,
-   fgs = "SETasGroups.csv",
+   fgs = "SETasGroupsDem_NoCep.csv",
    select_groups = groups,
    bboxes = boundary_boxes,
    check_acronyms = TRUE,
