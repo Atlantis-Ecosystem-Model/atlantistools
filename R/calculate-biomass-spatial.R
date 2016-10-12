9b983683cb4e93ea5d410dd42d37714333294a36
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-12 16:44:31 +0200

    Change parameter list.
    Work with dataframes generated with load_nc and load_nc_physics as soon as possible.

--- R/calculate-biomass-spatial.R
+++ R/calculate-biomass-spatial.R
@@ -7,59 +7,87 @@
 #' mgN is converted to t based on the stettings in the biol.prm file. Simulation time steps
 #' are converted to time in years based on output timesteps given in run.prm.
 #'
-#' @inheritParams preprocess
+#' @param nums Dataframe with information about numbers for age-based groups.
+#' Should be generated with \code{\link{load_nc}}.
+#' @param sn Dataframe with information about structural nitrogen for age-based groups.
+#' Should be generated with \code{\link{load_nc}}.
+#' @param rn Dataframe with information about reserve nitrogen for age-based groups.
+#' Should be generated with \code{\link{load_nc}}.
+#' @param n Dataframe with information about nitrogen for non-ge-based groups.
+#' Should be generated with \code{\link{load_nc}}.
+#' @param vol Dataframe with information about volume and layer height per polygon.
+#' Should be generated with \code{\link{load_nc_physics}}.
+#' @param bio_conv Numeric value to transform weight in mg N to tonnes.
+#' Should be generated with \code{\link{get_conv_mgnbiot}}.
+#' @param bps Vector of character strings giving the complete list of epibenthic
+#' functional groups (Only present in the sediment layer). The names have to match
+#' the column 'Name' in the 'functionalGroups.csv' file.
+#' Should be generated with \code{\link{load_bps}}.
 #' @return Dataframe with columns 'species', 'agecl', 'polygon', 'layer', 'time'.
 #' Biomass in [t] is stored in column 'atoutput'.
 #' @export
 #'
 #' @examples
-#' dir <- system.file("extdata", "gns", package = "atlantistools")
-#' nc_gen <- "outputNorthSea.nc"
-#' prm_biol <- "NorthSea_biol_fishing.prm"
-#' prm_run = "NorthSea_run_fishing_F.prm"
-#' bps = load_bps(dir, fgs = "functionalGroups.csv", init = "init_simple_NorthSea.nc")
-#' fgs = "functionalGroups.csv"
-#' bboxes = get_boundary(boxinfo = load_box(dir, bgm = "NorthSea.bgm"))
+#' # 1. Using preprocessed datasets.
+#' dir <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
+#' prm_biol <- "VMPA_setas_biol_fishing_Trunk.prm"
+#' bboxes <- get_boundary(boxinfo = load_box(dir, bgm = "VMPA_setas.bgm"))
+#' nc_gen <- "outputSETAS.nc"
+#' prm_run <- "VMPA_setas_run_fishing_F_Trunk.prm"
+#' fgs <- "SETasGroupsDem_NoCep.csv"
+#' bps <- load_bps(dir, fgs = fgs, init = "INIT_VMPA_Jan2015.nc")
 #'
-#' df <- calculate_biomass_spatial(dir, nc_gen, prm_biol, prm_run, bps, fgs, bboxes)
+#' bio_conv <- get_conv_mgnbiot(dir = dir, prm_biol = prm_biol)
+#' vol <- load_nc_physics(dir = dir, nc = nc_gen, select_physics = c("volume", "dz"),
+#'                        prm_run = prm_run, bboxes = bboxes, aggregate_layers = F)
+#'
+#' df <- calculate_biomass_spatial(nums = ref_nums, sn = ref_structn, rn = ref_resn, n = ref_n,
+#'                                 vol = vol, bio_conv = bio_conv, bps = bps)
+#'
+#' # 2. Read in dataframes from existing Atlantis simulation.
+#' groups_age <- c("Planktiv_S_Fish", "Pisciv_S_Fish")
+#' groups_rest <- c("Cephalopod", "Megazoobenthos", "Diatom", "Zoo", "Lab_Det", "Ref_Det")
+#'
+#' nums <- load_nc(dir = dir, nc = nc_gen, bps = bps, fgs = fgs,
+#'                 select_groups = groups_age, select_variable = "Nums",
+#'                 prm_run = prm_run, bboxes = bboxes)
+#' sn <- load_nc(dir = dir, nc = nc_gen, bps = bps, fgs = fgs,
+#'               select_groups = groups_age, select_variable = "StructN",
+#'               prm_run = prm_run, bboxes = bboxes)
+#' rn <- load_nc(dir = dir, nc = nc_gen, bps = bps, fgs = fgs,
+#'               select_groups = groups_age, select_variable = "ResN",
+#'               prm_run = prm_run, bboxes = bboxes)
+#' n <- load_nc(dir = dir, nc = nc_gen, bps = bps, fgs = fgs,
+#'              select_groups = groups_rest, select_variable = "N",
+#'              prm_run = prm_run, bboxes = bboxes)
+#'
+#' df <- calculate_biomass_spatial(nums = nums, sn = sn, rn = rn, n = n, vol = vol,
+#'                                 bio_conv = bio_conv, bps = bps)
+#'
+#' # 3. Read in dataframes from existing Atlantis simulation with Map().
+#' vars <- list("Nums", "StructN", "ResN", "N")
+#' grps <- list(groups_age, groups_age, groups_age, groups_rest)
+#' dfs <- Map(load_nc, select_variable = vars, select_groups = grps,
+#'            MoreArgs = list(dir = dir, nc = nc_gen, bps = bps, fgs = fgs,
+#'                            prm_run = prm_run, bboxes = bboxes))
+#'
+#' df <- calculate_biomass_spatial(nums = dfs[[1]], sn = dfs[[2]], rn = dfs[[3]], n = dfs[[4]],
+#'                                 vol = vol, bio_conv = bio_conv, bps = bps)
 
-calculate_biomass_spatial <- function(dir = getwd(), nc_gen, prm_biol, prm_run, bps, fgs, bboxes) {
-  fgs_data <- load_fgs(dir = dir, fgs = fgs)
+calculate_biomass_spatial <- function(nums, sn, rn, n, vol, bio_conv, bps) {
+  vol <- tidyr::spread_(vol, key_col = c("variable"), value_col = "atoutput")
 
-  groups <- get_groups(dir = dir, fgs = fgs)
-  groups_age <- get_age_groups(dir = dir, fgs = fgs)
-  groups_rest <- groups[!is.element(groups, groups_age)]
-
-  # 1st step: Load in data!
-  # - n per box and layer for each invert group
-  # - nums, resn, structn for each vert group per box, layer and ageclass
-  # - availability matrix
-  # - Volume per layer and box, layerthickness
-  # - convrsion factor mgN to bio t
-  vars <- list("StructN", "ResN", "Nums", "N")
-  grps <- list(groups_age, groups_age, groups_age, groups_rest)
-
-  data_bio <- Map(load_nc, select_variable = vars, select_groups = grps,
-                  MoreArgs = list(dir = dir, nc = nc_gen, bps = bps, fgs = fgs, prm_run = prm_run, bboxes = bboxes))
-  names(data_bio) <- tolower(vars)
-
-  vol <- load_nc_physics(dir = dir, nc = nc_gen, select_physics = c("volume", "dz"),
-                         prm_run = prm_run, bboxes = bboxes, aggregate_layers = F) %>%
-    tidyr::spread_(key_col = c("variable"), value_col = "atoutput")
-
-  bio_conv <- get_conv_mgnbiot(dir = dir, prm_biol = prm_biol)
-
-  # 2nd step: Calculate biomass per time, box and layer per group and ageclass!
+  # Calculate biomass per time, box and layer per group and ageclass!
   # - Age based groups!
-  names(data_bio$structn)[names(data_bio$structn) == "atoutput"] <- "sn"
-  names(data_bio$resn)[names(data_bio$resn) == "atoutput"] <- "rn"
-  biomass_age <- dplyr::inner_join(data_bio$nums, data_bio$structn, by = c("species", "agecl", "polygon", "layer", "time")) %>%
-    dplyr::left_join(data_bio$resn,  by = c("species", "agecl", "polygon", "layer", "time")) %>%
+  names(sn)[names(sn) == "atoutput"] <- "sn"
+  names(rn)[names(rn) == "atoutput"] <- "rn"
+  biomass_age <- dplyr::inner_join(nums, sn, by = c("species", "agecl", "polygon", "layer", "time")) %>%
+    dplyr::left_join(rn,  by = c("species", "agecl", "polygon", "layer", "time")) %>%
     dplyr::mutate_(.dots = stats::setNames(list(~(sn + rn) * atoutput * bio_conv), "atoutput")) %>%
     dplyr::select_(.dots = names(.)[!names(.) %in% c("sn", "rn")])
 
   # - Non age based groups!
-  biomass_pools <- dplyr::left_join(data_bio$n, vol, by = c("polygon", "layer", "time"))
+  biomass_pools <- dplyr::left_join(n, vol, by = c("polygon", "layer", "time"))
   biomass_pools$atoutput <- with(biomass_pools, ifelse(species %in% bps, atoutput * volume / dz * bio_conv, atoutput * volume * bio_conv))
   biomass_pools <- dplyr::select_(biomass_pools, .dots = c("species", "time", "polygon", "layer", "atoutput"))
   biomass_pools$agecl <- 1
