d9c10869187d607dea0e15d60d8b80d53cdef229
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-13 11:09:33 +0200

    Add explanations to utility functions.

--- R/utils-plot.R
+++ R/utils-plot.R
@@ -1,9 +1,11 @@
+# Customised nse version of facet_wrap used within atlantistools
 custom_wrap <- function(plot, col, ncol = 7) {
   plot <- plot + ggplot2::facet_wrap(stats::as.formula(paste("~", paste(col, collapse = "+"))),
                                      scales = "free_y", ncol = ncol, labeller = ggplot2::label_wrap_gen(width = 15))
   return(plot)
 }
 
+# Customised nse version of facet_grid used within atlantistools
 custom_grid <- function(plot, grid_x, grid_y) {
   px <- paste(grid_x, collapse = "+")
   py <- paste(grid_y, collapse = "+")
@@ -12,19 +14,22 @@ custom_grid <- function(plot, grid_x, grid_y) {
   return(plot)
 }
 
+# Customised function to simplify nse mapping of x- and y variable.
+# Additional mappings may be added here. However, x- and y are the most fundamental ones...
 custom_map <- function(data, x, y) {
   plot <- ggplot2::ggplot(data = data, ggplot2::aes_(x = lazyeval::interp(~var, var = as.name(x)),
                                                      y = lazyeval::interp(~var, var = as.name(y))))
   return(plot)
 }
 
+# Changes applied to every ggplot object within atlantistools.
 ggplot_custom <- function(plot) {
   plot <- plot + ggplot2::coord_cartesian(expand = FALSE)
-  plot <- plot + ggplot2::scale_y_continuous(labels = abbreviate)
+  plot <- plot + ggplot2::scale_y_continuous(labels = scales::scientific(x, digits = 2))
   return(plot)
 }
 
-
+# Function to add data range to calibration plot!
 plot_add_box <- function(plot, range = c(0.5, 0.2)) {
   plot <- plot + ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = 1 - range[1], ymax = 1 + range[1], alpha = 0.1)
   plot <- plot + ggplot2::annotate("rect", xmin = -Inf, xmax = Inf, ymin = 1 - range[2], ymax = 1 + range[2], alpha = 0.3)
