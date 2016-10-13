13207d0c7654087c8d9fb79b8a60a855278f6670
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-13 10:44:54 +0200

    Add custom_grid function.

--- R/utils-plot.R
+++ R/utils-plot.R
@@ -1,6 +1,14 @@
 custom_wrap <- function(plot, col, ncol = 7) {
-  plot <- plot + ggplot2::facet_wrap(as.formula(paste("~", paste(col, collapse = "+"))),
+  plot <- plot + ggplot2::facet_wrap(stats::as.formula(paste("~", paste(col, collapse = "+"))),
                                      scales = "free_y", ncol = ncol, labeller = ggplot2::label_wrap_gen(width = 15))
+  return(plot)
+}
+
+custom_grid <- function(plot, grid_x, grid_y) {
+  px <- paste(grid_x, collapse = "+")
+  py <- paste(grid_y, collapse = "+")
+  plot <- plot + ggplot2::facet_wrap(stats::as.formula(paste(px, "~", py)),
+                                     scales = "free", labeller = ggplot2::label_wrap_gen(width = 15))
   return(plot)
 }
 
@@ -14,5 +22,11 @@ ggplot_custom <- function(plot) {
   plot <- plot + ggplot2::coord_cartesian(expand = FALSE)
   plot <- plot + ggplot2::scale_y_continuous(labels = abbreviate)
   return(plot)
+}
+
+
+plot_add_box <- function(plot) {
+
+
 }
 
