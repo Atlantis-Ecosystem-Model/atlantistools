581b13a648caa9fb0f63df7723f0aa58d7c54bb1
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-18 14:49:04 +0200

    Add preprocess reference dataframe.
    The dataframe is based on the vignette 'model-preprocess' and 'data-vignette-model-preprocess.R' in /raw-data.

---
+++ R/data-preprocess.R
@@ -0,0 +1,25 @@
+#' preprocess-setas
+#'
+#' See "data-vignette-model-preprocess.R" and "model-preprocess.Rmd" for further information.
+#'
+#' @format List with 17 dataframes.
+#' \enumerate{
+#'   \item biomass
+#'   \item biomass_age
+#'   \item biomass_consumed
+#'   \item diet
+#'   \item eat_age
+#'   \item flux
+#'   \item grazing
+#'   \item growth_age
+#'   \item nums
+#'   \item nums_age
+#'   \item nums_box
+#'   \item physics
+#'   \item resn_age
+#'   \item sink
+#'   \item spatial_overlap
+#'   \item ssb_rec
+#'   \item structn_age
+#'     }
+"preprocess"
