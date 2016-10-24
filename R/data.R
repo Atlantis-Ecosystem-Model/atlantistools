025cbd7c0236aca2e0c91c4f6d9dd18026dd83cb
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-24 11:13:19 +0200

    Add documentation for exported dataframes.

--- R/data.R
+++ R/data.R
@@ -193,6 +193,35 @@
 #' }
 "ref_agemat"
 
+#' Consumed biomass.
+#'
+#' @format
+#' \describe{
+#' \item{pred}{Name of the functional groups given as character string.
+#' The names match with the column 'LongName' in the functionalGroups.csv file.}
+#' \item{agecl}{Ageclass given as integer from 1 to NumCohorts.}
+#' \item{polygon}{Boxid starting from 0 to numboxes - 1.}
+#' \item{time}{Simulation time in years. Modeltimestep was converted to actual
+#' time based on the settings in the 'run.prm' file.}
+#' \item{prey}{Name of the functional groups given as character string.
+#' The names match with the column 'LongName' in the functionalGroups.csv file.}
+#' \item{atoutput}{Obersavtion column storing the actual output value.
+#' Consumed biomass in tonnes.}
+#' }
+"ref_bio_cons"
 
-
-
+#' Sparial biomass.
+#'
+#' @format
+#' \describe{
+#' \item{species}{Name of the functional groups given as character string.
+#' The names match with the column 'LongName' in the functionalGroups.csv file.}
+#' \item{agecl}{Ageclass given as integer from 1 to NumCohorts.}
+#' \item{polygon}{Boxid starting from 0 to numboxes - 1.}
+#' \item{layer}{Layerid starting from 0 to numlayers - 1.}
+#' \item{time}{Simulation time in years. Modeltimestep was converted to actual
+#' time based on the settings in the 'run.prm' file.}
+#' \item{atoutput}{Obersavtion column storing the actual output value.
+#' Biomass in tonnes.}
+#' }
+"ref_bio_sp"
