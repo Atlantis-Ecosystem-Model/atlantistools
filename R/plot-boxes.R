8bd40dce334983cc11d4fbb1362cc959c5147e06
alketh <Alexander.Keth@uni-hamburg.de>
2016-10-28 10:31:35 +0200

    Change defaults in plot_boxes.

--- R/plot-boxes.R
+++ R/plot-boxes.R
@@ -2,25 +2,37 @@
 #'
 #' @param data Dataframe to be plotted.
 #' @return ggplot2 object
+#' @return color_boxes logical indicating if polygons should be color coded or not.
 #' @export
 #' @family plot functions
 #'
 #' @examples
 #' d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
 #' bgm_data <- convert_bgm(dir = d, bgm = "VMPA_setas.bgm")
+#'
+#' # Use color coding for polygons.
 #' plot_boxes(bgm_data)
+#'
+#' # Only use text to indicate polygons.
+#' plot_boxes(bgm_data, color_boxes = FALSE)
 
-plot_boxes <- function(data) {
+plot_boxes <- function(data, color_boxes = TRUE) {
   check_df_names(data = data, expect = c("long", "lat", "polygon", "inside_lat", "inside_long"))
 
   inside <- unique(subset(data, select = c("inside_lat", "inside_long", "polygon")))
   names(inside)[1:2] <- c("lat", "long")
-  plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~long, y = ~lat, fill = ~factor(polygon), group = ~factor(polygon), label = ~polygon)) +
-    ggplot2::geom_polygon(colour = "black") +
+
+  plot <- ggplot2::ggplot(data, ggplot2::aes_(x = ~long, y = ~lat, group = ~factor(polygon), label = ~polygon)) +
+    ggplot2::geom_polygon(colour = "black", fill = "white") +
     ggplot2::geom_text(data = inside) +
-    theme_atlantis() +
-    ggplot2::guides(fill = ggplot2::guide_legend(ncol = 3)) +
-    ggplot2::theme(legend.position = "right")
+    ggplot2::theme_void()
+
+  if (color_boxes) {
+    plot <- plot + ggplot2::aes_(fill = ~factor(polygon))
+    plot <- plot + ggplot2::theme(legend.position = "none")
+  } else {
+    # plot <- plot + ggplot2::geom_polygon(fill = "blue", alpha = 0.1)
+  }
 
   return(plot)
 }
