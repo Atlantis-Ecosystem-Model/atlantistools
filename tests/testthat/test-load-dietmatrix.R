805a8debdc47a873679e141af8512f62c62a4b79
alketh <Alexander.Keth@uni-hamburg.de>
2016-11-04 11:57:18 +0100

    Add test for bec_dev models.

--- tests/testthat/test-load-dietmatrix.R
+++ tests/testthat/test-load-dietmatrix.R
@@ -4,6 +4,10 @@ dm <- load_dietmatrix(dir = system.file("extdata", "setas-model-new-trunk", pack
                       prm_biol = "VMPA_setas_biol_fishing_Trunk.prm",
                       fgs = "SETasGroupsDem_NoCep.csv")
 
+dm2 <- load_dietmatrix(dir = system.file("extdata", "setas-model-new-becdev", package = "atlantistools"),
+                       prm_biol = "VMPA_setas_biol_fishing_New.prm",
+                       fgs = "SETasGroups.csv", version_flag = 1)
+
 test_that("test output numbers", {
   expect_equal(length(unique(dm$code)), 13)
   expect_equal(dm$avail[dm$pred == "FVS" & dm$prey_stanza == 1 & dm$pred_stanza == 2 & dm$prey == "PL"], 0.02)
