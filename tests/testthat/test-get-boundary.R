3e46155f0301ea20e87735dbefb708b353e517d8
alketh <Alexander.Keth@uni-hamburg.de>
2016-11-16 17:16:01 +0100

    boundary box extraction does work for dec_dev.
    So does load_dietmatrix.

--- tests/testthat/test-get-boundary.R
+++ tests/testthat/test-get-boundary.R
@@ -7,3 +7,11 @@ boxinfo <- load_box(dir = d, bgm = "VMPA_setas.bgm")
 test_that("test output of get_boundary", {
   expect_equal(get_boundary(boxinfo = boxinfo), c(0, 6, 7, 8, 9, 10))
 })
+
+
+# test for bec_dev models
+test_that("test output of get_boundary", {
+  expect_equal(get_boundary(load_box(dir = system.file("extdata", "setas-model-new-becdev", package = "atlantistools"), bgm = "VMPA_setas.bgm")), c(0, 6, 7, 8, 9, 10))
+})
+
+
