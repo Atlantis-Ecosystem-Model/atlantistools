9fc1148ff4f85bba5f7cf72d50b3ee87d8ddc5a8
alketh <Alexander.Keth@uni-hamburg.de>
2016-12-07 17:11:12 +0100

    layers fliped for different data inputs.

--- R/flip-layers.R
+++ R/flip-layers.R
@@ -8,15 +8,24 @@
 #' @examples
 #' data <- rbind(expand.grid(species = "sp1", polygon = 0, layer = 0:7),
 #'               expand.grid(species = "sp1", polygon = 1, layer = 0:4),
-#'               expand.grid(species = "sp1", polygon = 2, layer = 0:2))
+#'               expand.grid(species = "sp1", polygon = 2, layer = 0:2),
+#'               expand.grid(species = "sp1", polygon = 3, layer = c(0:3, 7)))
 #' data$atoutput <- runif(nrow(data), min = 0, max = 2)
 #' flip_layers(data)
 
 flip_layers <- function(data) {
   if (any(!c("polygon", "layer") %in% names(data))) stop("Columns polygon and layer not present in data.")
   df <- unique(dplyr::select_(data, .dots = c("polygon", "layer")))
-  layers <- lapply(split(df, df$polygon), function(x) x$layer)
-  df$layer <- unlist(lapply(layers, function(x) rev(x) + 1))
+  sediment <- max(df$layer)
+  layers <- split(df, df$polygon)
+
+  # for(i in seq_along(layers)) {
+  #   if()
+  # }
+  #
+  # , function(x) sort(x$layer))
+
+  df$layer_fliped <- unlist(lapply(layers, function(x) rev(x) + 1))
 
   data_fliped <- data %>%
     dplyr::left_join(df, by = "polygon") %>%
@@ -24,3 +33,16 @@ flip_layers <- function(data) {
   data_fliped$layer.x <- NULL
   return(data_fliped)
 }
+
+add_fliped_layers <- function(df, sed) {
+  df <- dplyr::arrange_(df, ~layer)
+  if (any(df$layer == sed)) {
+    layer_fliped <- c(rev(df$layer[df$layer != sed]), sed) + 1
+  } else {
+    layer_fliped <- rev(df$layer) + 1
+  }
+  df$layer_fliped <- layer_fliped
+  return(df)
+}
+
+
