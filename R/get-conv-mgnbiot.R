a279a88108c4a02d7fb0c2cfccaa489123f8740d
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-14 13:09:42 +0100

    Remove 'dir' from get_conv_mgnbiot.

--- R/get-conv-mgnbiot.R
+++ R/get-conv-mgnbiot.R
@@ -1,25 +1,21 @@
 #' Extract conversion factor used to transform data from nitrogen in mg to
 #' biomass in tonnes.
 #
-#' @param dir Character string giving the path of the Atlantis model folder.
-#' If data is stored in multiple folders (e.g. main model folder and output
-#' folder) you should use 'NULL' as dir.
-#' @param prm_biol Character string giving the filename of the biological
-#' parameterfile. Usually "[...]biol_fishing[...].prm". In case you are using
-#' multiple folders for your model files and outputfiles pass the complete
-#' folder/filename string and set dir to 'NULL'.
+#' @inheritParams extract_prm
 #' @return Conversion factor as numeric value.
 #' @family get functions
 #'
 #' @examples
 #' d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
-#' get_conv_mgnbiot(dir = d, prm_biol = "VMPA_setas_biol_fishing_Trunk.prm")
+#' prm_biol <- file.path(d, "VMPA_setas_biol_fishing_Trunk.prm")
+#'
+#' get_conv_mgnbiot(prm_biol)
 
 #' @export
 
-get_conv_mgnbiot <- function(dir = getwd(), prm_biol){
-  x_cn <- extract_prm(dir = dir, prm_biol = prm_biol, variables = "X_CN")
-  k_wetdry <- extract_prm(dir = dir, prm_biol = prm_biol, variables = "k_wetdry")
+get_conv_mgnbiot <- function(prm_biol){
+  x_cn <- extract_prm(prm_biol = prm_biol, variables = "X_CN")
+  k_wetdry <- extract_prm(prm_biol = prm_biol, variables = "k_wetdry")
   conv <- x_cn * k_wetdry / 1000000000
   return(conv)
 }
