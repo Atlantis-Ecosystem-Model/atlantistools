5ba6bc5c570e81d28da33e10abeb041ad3d8e4ac
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-14 15:03:21 +0100

    Fix test.

--- tests/testthat/test-get-boundary.R
+++ tests/testthat/test-get-boundary.R
@@ -13,7 +13,7 @@ d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
 boxinfo <- load_box(bgm = file.path(d, "VMPA_setas.bgm"))
 
 test_that("test output of get_boundary", {
-  expect_equal(get_boundary(load_box(boxinfo), c(0, 6, 7, 8, 9, 10))
+  expect_equal(get_boundary(boxinfo = boxinfo), c(0, 6, 7, 8, 9, 10))
 })
 
 
