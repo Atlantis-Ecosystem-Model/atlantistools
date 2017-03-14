602044c929b6d647a78dfe3ee8e28ae088566244
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-14 12:04:14 +0100

    Remove 'dir' in load_fgs.
    Use as default documentation for parameter 'fgs'.

--- R/load-fgs.R
+++ R/load-fgs.R
@@ -2,7 +2,6 @@
 #'
 #' Read in the functional group file as dataframe.
 #'
-#' @param dir Path of the Atlantis model folder.
 #' @param fgs Name of the functional groups file which is typically saved as
 #' \code{functionalGroups.csv}.
 #'
@@ -14,18 +13,16 @@
 #' @examples
 #' d <- system.file("extdata", "setas-model-new-becdev", package = "atlantistools")
 #' file <- "SETasGroups.csv"
-#' fgs <- load_fgs(d, file)
+#' fgs <- load_fgs(file.path(d, file))
 #' head(fgs)
 #'
 #' d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
 #' file <- "SETasGroupsDem_NoCep.csv"
-#' fgs <- load_fgs(d, file)
+#' fgs <- load_fgs(file.path(d, file))
 #' head(fgs)
 
-load_fgs <- function(dir = getwd(), fgs) {
-  if (!is.null(dir)) fgs <- file.path(dir, fgs)
-  result <- utils::read.table(file = fgs, sep = ",",
-                       header = TRUE, stringsAsFactors = FALSE)
+load_fgs <- function(fgs) {
+  result <- utils::read.table(file = fgs, sep = ",", header = TRUE, stringsAsFactors = FALSE)
   # Rename column longname if space is present in raw file.
   names(result)[names(result) == "Long.Name"] <- "LongName"
   return(result)
