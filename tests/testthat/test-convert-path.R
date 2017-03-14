8b47852d1c10fff12bbc6d760e871f906341f245
alketh <Alexander.Keth@uni-hamburg.de>
2017-03-14 10:56:35 +0100

    Remove 'convert_path'.

--- tests/testthat/test-convert-path.R
+++
@@ -1,6 +0,0 @@
-context("convert_path test equality of calls")
-
-test_that("Test if paths are identical", {
-  expect_equal(convert_path(dir = file.path("c:", "atlantis-model"), file = "functionalGroups.csv"),
-               convert_path(dir = NULL, file = file.path("c:", "atlantis-model", "functionalGroups.csv")))
-})
