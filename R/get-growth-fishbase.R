5082e0e8b24433231e366644fb0bf69228f59f73
alketh <Alexander.Keth@uni-hamburg.de>
2017-04-13 14:57:25 +0200

    Change refid extraction.

--- R/get-growth-fishbase.R
+++ R/get-growth-fishbase.R
@@ -15,9 +15,14 @@
 #' df <- get_growth_fishbase(fish)
 #' head(df)
 #'
+#' df <- get_growth_fishbase(fish, mirror = "de")
+#' head(df)
+
 #' # Only use for debugging purposes.
 #' \dontrun{
 #' fish <- read.csv("Z:/my_data_alex/fish_species_names_from_ibts.csv", stringsAsFactors = FALSE)[, 1]
+#' url <- get_growth_fishbase(fish)
+#' url <- urls$ref_url
 #' }
 
 #' @export
@@ -38,7 +43,7 @@ get_growth_fishbase <- function(fish, mirror = "se"){
   pos_missing <- c(pos_missing)
   if (length(pos_missing) >= 1) {
     missing_species <- sort(names(ids)[pos_missing])
-    warning("No growth information available:\n", paste(missing_species, collapse = "\n "))
+    warning(paste("No growth information available for", length(pos_missing), "species:\n"), paste(missing_species, collapse = "\n"))
     ids <- ids[-pos_missing]
     fishbase <- fishbase[-pos_missing]
     urls <- urls[-pos_missing]
@@ -50,12 +55,12 @@ get_growth_fishbase <- function(fish, mirror = "se"){
     purrr::map(., 3)
 
   # add names to dataframes
-  result <- purrr::map2_df(.x = result, .y = fish, ~tibble::add_column(.x, rep(.y, times = nrow(.x)))) %>%
+  result <- purrr::map2(.x = result, .y = names(ids), ~tibble::add_column(.x, rep(.y, times = nrow(.x)))) %>%
+    do.call(rbind, args = .) %>% # rbind is necessary due to different col-classes in 'Sex' = 'chr' and 'logical'
     purrr::set_names(., c("xxx", "linf", "length_type", "k", "to", "sex", "m", "temp", "lm", "a",
                           "country", "locality", "questionable", "captive", "species"))
 
   # Cleanup
-  # Convert chr columns to numeric if possible.
   result$xxx <- NULL
   result[result == ""] <- NA
 
@@ -65,11 +70,14 @@ get_growth_fishbase <- function(fish, mirror = "se"){
     purrr::map(., ~rvest::html_attr(., "href")) %>%
     purrr::map(., ~.[stringr::str_detect(., pattern = "FishPopGrowthSummary")])
 
-  # check if result and urls match.
+  # check if result and urls match. Rearrange due to alphabetical ordering in df.
   count <- agg_data(result, col = "linf", groups = "species", out = "count", fun = length)
+  count <- count[match(names(ids), count$species), ]
   if (all(count$count == purrr::map_int(ref_urls, length))) {
     result$ref_url <- unlist(ref_urls)
-    result$ref_id <- purrr::map(result$ref_url, url_to_refid)
+    ref_ids <- purrr::map(result$ref_url, url_to_refid)
+    result$main_ref <- purrr::map_int(ref_ids, 1)
+    result$data_ref <- purrr::map_int(ref_ids, 1)
   } else {
     warning("ref_urls and final table do not match.")
   }
@@ -81,15 +89,21 @@ get_growth_fishbase <- function(fish, mirror = "se"){
 url_to_refid <- function(url, mirror = "se") {
   # extract links from html
   links <- xml2::read_html(paste0("http://www.fishbase.", mirror, "/", url)) %>%
-    rvest::html_nodes(., "a") %>%
-    rvest::html_attr(., "href")
+    rvest::html_text(.)
 
-  # extract reference ids
-  ids <- links[which(stringr::str_detect(links, pattern = "References"))[1:2]] %>%
-    stringr::str_split(., pattern = "ID=") %>%
-    purrr::map_int(., ~as.integer(.[2]))
+  # this is a bit ugly but it works like a charm.
+  p1 <- stringr::str_split_fixed(links, pattern = "Main Ref. :", n = 2)[, 2]
+  p2 <- stringr::str_split_fixed(p1, pattern = "Data Ref. :", n = 2)
+  p3 <- stringr::str_split_fixed(p2[, 2], pattern = "Data Type :", n = 2)
 
-  return(unique(ids))
+  # combine main and data ref strings
+  refs <- c(p2[, 1], p3[, 1])
+
+  # extract numeric values
+  ref_id <- purrr::map_chr(refs, ~paste0(unlist(stringr::str_extract_all(string = ., pattern = "[0-9]")), collapse = ""))
+  ref_id <- suppressWarnings(as.integer(ref_id))
+
+  return(ref_id)
 }
 
 
