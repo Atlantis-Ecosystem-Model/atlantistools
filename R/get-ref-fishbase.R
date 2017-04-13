809f6d1ee7296c2a7da692a0c01674cd1ab1963f
alketh <Alexander.Keth@uni-hamburg.de>
2017-04-13 15:31:52 +0200

    Change code to match with new fb functions.

--- R/get-ref-fishbase.R
+++ R/get-ref-fishbase.R
@@ -11,16 +11,21 @@
 #' fish <- c("Gadus morhua", "Merlangius merlangus")
 #' growth_fishbase <- get_growth_fishbase(fish)
 #' get_ref_fishbase(growth_fishbase)
+#' growth_fishbase <- get_growth_fishbase("Scyliorhinus canicula")
+#'
 
 get_ref_fishbase <- function(growth_fishbase, mirror = "se") {
   # extract unique reference and species combinations
-  clean_ref <- dplyr::select_(growth_fishbase, .dots = c("species", "ref_id")) %>%
-    split(., .$species) %>%
-    purrr::map(., ~unique(unlist(.$ref_id))) %>%
-    purrr::map(., ~.[!is.na(.)])  # remove NAs
+  clean_ref <- dplyr::select_(growth_fishbase, .dots = c("species", "main_ref", "data_ref"))
 
-  # convert to tibble
-  clean_ref <- purrr::map2_df(.x = names(clean_ref), .y = clean_ref, ~tibble::tibble(species = .x, ref_id = .y))
+  # remove duplicated data_ref entries
+  clean_ref$data_ref[clean_ref$data_ref == clean_ref$main_ref] <- NA
+  clean_ref <- tidyr::gather_(data = clean_ref, key_col = "ref_type", value_col = "ref_id", gather_cols = c("main_ref", "data_ref"), na.rm = TRUE)
+
+  # calculate perc ref entry and remove duplicates!
+  clean_ref$perc <- 1
+  agg_ref <- agg_perc(clean_ref, col = "perc", groups = c("species"), out = "perc") %>%
+    agg_data(., col = "perc", groups = c("species", "ref_type", "ref_id"), out = "perc", fun = sum)
 
   # Extract data from fishbase.org
   ref <- purrr::map_chr(clean_ref$ref_id, ~paste0("http://www.fishbase.", mirror, "/References/FBRefSummary.php?ID=", .)) %>%
