02e4b5a8ecaec041d9303dc0d100f00107b06d37
alketh <Alexander.Keth@uni-hamburg.de>
2017-05-04 16:00:21 +0200

    Recreate preprocess.

--- data-raw/data-vignette-model-preprocess.R
+++ data-raw/data-vignette-model-preprocess.R
@@ -1,14 +1,14 @@
 library("rmarkdown")
 
-d <- system.file("vignettes", package = "atlantistools")
+dir <- file.path(getwd(), "vignettes")
 
-render(input = file.path(d, "model-preprocess.Rmd"))
+render(input = file.path(dir, "model-preprocess.Rmd"))
 
 preprocess <- result
 
 devtools::use_data(preprocess, overwrite = TRUE)
 
-file.remove(file.path(d, "model-preprocess.html"))
+file.remove(file.path(dir, "model-preprocess.html"))
 
 rm(list = ls())
 
