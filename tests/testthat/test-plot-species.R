7f6abec8e99ce76a48e8f200dbd84b504eda7b83
alketh <Alexander.Keth@uni-hamburg.de>
2017-05-05 11:45:15 +0200

    Implement first visual test with vdiffr.

--- tests/testthat/test-plot-species.R
+++ tests/testthat/test-plot-species.R
@@ -3,9 +3,11 @@ context("plot-species")
 # Add some etsting due to failures of change_theme()
 p <- plot_line(preprocess$biomass)
 
-p <- p + ggplot2::theme(legend.position = "none")
-p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank())
-p <- p + ggplot2::theme(strip.text.x = ggplot2::element_blank())
-p <- p + ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5))
+# p <- p + ggplot2::theme(legend.position = "none")
+# p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank())
+# p <- p + ggplot2::theme(strip.text.x = ggplot2::element_blank())
+# p <- p + ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5))
 
-
+test_that("check visually", {
+  vdiffr::expect_doppelganger("line plot", p)
+})
