b8fdf7fe0ed83946a21409f031629781e6724560
alketh <Alexander.Keth@uni-hamburg.de>
2017-08-16 18:12:47 +0200

    Split biology references.

--- R/get-ref-biotoc.R
+++ R/get-ref-biotoc.R
@@ -20,23 +20,40 @@ get_ref_biotic <- function(taxon) {
     } else {
       url <- taxon
     }
-  url <- paste0("http://www.marlin.ac.uk/biotic/browse.php?sp=x&spn=", url)
+    url <- paste0("http://www.marlin.ac.uk/biotic/browse.php?sp=x&spn=", url)
 
-  ref_raw <- xml2::read_html(url)
+    # Read in the url as raw html.
+    ref_raw <- xml2::read_html(url)
 
-  # Extract reference by category
-  ref_df <- rvest::html_table(ref_raw, fill = TRUE)[[1]]
-  ref_df <- ref_df[grepl(ref_df[, 1], pattern = "References"), ]
-  ref_df <- ref_df[, purrr::map_lgl(ref_df, ~sum(is.na(.)) != nrow(ref_df))]
+    # Extract reference by category
+    ref_df <- rvest::html_table(ref_raw, fill = TRUE)[[1]][, 1:2]
+    ref_df <- ref_df[grepl(ref_df[, 1], pattern = "References"), ]
 
-  wawa <- wuwu[[1]][!is.na(wuwu[[1]])]
+    # Get the urls to the reference metadata (author, year, title)
+    ref_urls <- rvest::html_nodes(ref_raw, "a") %>%
+      rvest::html_attr(., "href")
+    ref_urls <- ref_urls[grepl(ref_urls, pattern = "references")]
 
-  ref_urls <- rvest::html_nodes(ref_raw, "a") %>%
-    rvest::html_attr(., "href")
-  ref_urls <- ref_urls[grepl(ref_urls, pattern = "references")]
+    wewe <- rvest::html_text(ref_raw)
 
-  wewe <- rvest::html_text(ref_raw)
+    bio_ref <- function(ref_raw) {
+      # Get the section headings of "General Biology Additional Information"
+      headings <- rvest::html_nodes(ref_raw, "b") %>%
+        rvest::html_text(.)
+      headings <- headings[headings != "Note"]
 
+      # Split the text into subsections
+      biology <- rvest::html_text(ref_raw)
+      biol_start <- stringr::str_locate(biology, "General Biology Additional Information")[1, 2]
+      biol_end   <- stringr::str_locate(biology, "Biology References")[1, 1]
+      biology <- stringr::str_sub(biology, start = biol_start + 1, end = biol_end - 1)
+
+      bio <- tibble::tibble()
+      biology <- readLines(url)
+      biol_start <- grep(pattern = "General Biology Additional Information", biology)
+      biol_end   <- grep(pattern = "Biology References", biology)
+      biology <- biology[biol_start:(biol_end - 1)]
+    }
   }
   # Read information from Biotic
 }
