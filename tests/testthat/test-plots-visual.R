ed184f4004168b94cb128f027616a47dc2f9167e
alketh <Alexander.Keth@uni-hamburg.de>
2017-08-22 11:03:40 +0200

    Add in test roadmap.

--- tests/testthat/test-plots-visual.R
+++ tests/testthat/test-plots-visual.R
@@ -1,7 +1,15 @@
 context("plot-species")
 
-# Add some etsting due to failures of change_theme()
-p <- plot_line(preprocess$biomass)
+# Add plots for visual testing here
+p1 <- plot_line(preprocess$biomass)
+
+# General roadmap from INDperform
+# How to implement test after the function is written:
+# 1. Define result and test if function call is identical
+# 2. Use different input objects (e.g. with NAs, without NAs, with Zeros, differemt object types (vector vs. array))
+# 3. Test the object structure of the output with typeof or class
+# 4. Test column classes in a tibble/dataframe of the returned object.
+# 5. Test every warning() and stop() command with expect_warning(), expect_error().
 
 # p <- p + ggplot2::theme(legend.position = "none")
 # p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank())
@@ -9,5 +17,6 @@ p <- plot_line(preprocess$biomass)
 # p <- p + ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5))
 
 test_that("check visually", {
-  vdiffr::expect_doppelganger("line plot preprocess$biomass", p)
+  vdiffr::expect_doppelganger("line plot preprocess$biomass", p1)
+  vdiffr::expect_doppelganger("plot consumed biomass ref_bio_cons", plot_consumed_biomass(ref_bio_cons))
 })
