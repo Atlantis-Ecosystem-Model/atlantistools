d9058f4408710c2b9d1a4a95519eb525cc6795ec
alketh <Alexander.Keth@uni-hamburg.de>
2017-08-24 09:20:59 +0200

    Add test for plot_spatial_overlap.

--- tests/testthat/test-plots-visual.R
+++ tests/testthat/test-plots-visual.R
@@ -9,6 +9,7 @@ df <- load_spec_mort(specmort, prm_run, fgs, version_flag = 1)
 plots <- plot_diet_bec_dev(df, wrap_col = "agecl")
 plots <- plots[[4]]
 ex_data <- read.csv(file.path(d, "setas-ssb-rec.csv"), stringsAsFactors = FALSE)
+sp_overlap <- calculate_spatial_overlap(ref_bio_sp, ref_dietmatrix, ref_agemat)
 
 # Add plots for visual testing here
 p1 <- plot_line(preprocess$biomass)
@@ -22,6 +23,7 @@ p5 <- function() gridExtra::grid.arrange(dummy)
 
 p6 <- plot_bar(preprocess$nums_age, fill = "agecl", wrap = "species")
 p7 <- plot_rec(preprocess$ssb_rec, ex_data)
+p8 <- plot_spatial_overlap(sp_overlap)
 
 # General roadmap from INDperform: How to implement a visual test
 # 1. Add new refernce with (svg-file is created in tests/ffigs/subfolder)
@@ -49,4 +51,5 @@ test_that("check visually", {
   # vdiffr::expect_doppelganger("line plot preprocess$biomass twice", p5)
   vdiffr::expect_doppelganger("plot bar preprocess$nums_age", p6)
   vdiffr::expect_doppelganger("plot rec preprocess$ssb_rec ex_data", p7)
+  vdiffr::expect_doppelganger("plot spatial overlap sp_overlap", p7)
 })
