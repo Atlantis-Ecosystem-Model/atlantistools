35f822508fb2d05bd23f716ce3958cedd2ffe7d0
andybeet <22455149+andybeet@users.noreply.github.com>
2026-04-28 14:32:31 -0400

    style: format with air 0.9.0 after update from 0.7.0

--- data-raw/data-reference.R
+++ data-raw/data-reference.R
@@ -1,17 +1,17 @@
 # Dataframes related to reference extraction.
 
 ref_lit <- tibble::tribble(
-  ~year,
-  ~author,
-  ~title,
-  2008,
-  "Gislason, H., N. Daan, J.C. Rice and J.G. Pope",
-  "Does natural mortality depend on individual size?. ICES CM 2008/F:16.",
-  1974,
-  "Daan, N.",
-  "Growth of the North Sea cod, Gadus morhua. Neth. J. Sea Res. 8(1):27-48.",
-  1951,
-  "Chang, H.-W.",
+  ~year                                                                           ,
+  ~author                                                                         ,
+  ~title                                                                          ,
+                                                                             2008 ,
+  "Gislason, H., N. Daan, J.C. Rice and J.G. Pope"                                ,
+  "Does natural mortality depend on individual size?. ICES CM 2008/F:16."         ,
+                                                                             1974 ,
+  "Daan, N."                                                                      ,
+  "Growth of the North Sea cod, Gadus morhua. Neth. J. Sea Res. 8(1):27-48."      ,
+                                                                             1951 ,
+  "Chang, H.-W."                                                                  ,
   "Age and growth of Callionymus lyra L. J. Mar. Biol. Asoc. U.K. 30(2):281-296."
 )
 
