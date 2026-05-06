310e64a021932096bab0b58400f52be9cc8e26eb
andybeet <22455149+andybeet@users.noreply.github.com>
2026-05-06 13:59:16 -0400

    fix(progress_bar): removed defunct progress bar

--- R/load-nc.R
+++ R/load-nc.R
@@ -209,20 +209,13 @@ load_nc <- function(
   # Initialise progress par!
   # if (report) pb <- txtProgressBar(min = 0, max = length(search_clean), style = 3)
   if (report) {
-    pb <- dplyr::progress_estimated(length(search_clean))
+    message(paste0("Reading in the nc file: ", nc))
   }
   for (i in seq_along(search_clean)) {
     at_data[[i]] <- RNetCDF::var.get.nc(
       ncfile = at_out,
       variable = search_clean[i]
     )
-    # update progress bar
-    # if (report) setTxtProgressBar(pb, i)
-    if (report) pb$tick()$print()
-  }
-  # if (report) close(pb)
-  if (report) {
-    pb$stop()
   }
 
   # at_data <- lapply(search_clean, RNetCDF::var.get.nc, ncfile = at_out)
