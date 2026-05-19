1bf535dd93083c9646ba4ad2a48e0979f9cda462
andybeet <22455149+andybeet@users.noreply.github.com>
2026-05-19 10:51:11 -0400

    tests(remove CRAN): prevent Cran related content in testing

--- tests/testthat/helper-fontconfig.R
+++ tests/testthat/helper-fontconfig.R
@@ -2,6 +2,3 @@
 on_appveyor <- function() {
   identical(Sys.getenv("APPVEYOR"), "True")
 }
-on_cran <- function() {
-  !identical(Sys.getenv("NOT_CRAN"), "true")
-}
--- tests/testthat/test-load-spec-mort.R
+++ tests/testthat/test-load-spec-mort.R
@@ -1,7 +1,7 @@
 context("test load_spec_mort")
 
 d <- system.file("extdata", "setas-model-new-trunk", package = "atlantistools")
-specmort <- file.path(d, "outputSETASSpecificPredMort.txt")
+specmort <- file.path(d, "outputSETASSpecificMort.txt")
 prm_run <- file.path(d, "VMPA_setas_run_fishing_F_Trunk.prm")
 fgs <- file.path(d, "SETasGroupsDem_NoCep.csv")
 
@@ -11,7 +11,7 @@ test_that("test specific values", {
   expect_equal(class(df)[1], "tbl_df")
   expect_equivalent(
     sapply(df, class),
-    c("numeric", "character", "numeric", "character", "numeric")
+    c("double", "character", "double", "character", "double")
   )
   expect_true(all(df$time <= 5))
   # expect_identical(df$atoutput[df$prey == "FPS" & df$pred == "DL" & df$time == 0.2], 6.801727e-009) not posible with new 365 day output
